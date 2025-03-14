0f945b5fa258d38d5ab1e5ae66fa1a6ccbbafa06
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-03-14 21:03:36 +0900

    ch01/05

--- ch01/ex05/index.test.js
+++ ch01/ex05/index.test.js
@@ -17,6 +17,26 @@ describe("math", () => {
       expect(abs(0)).toBe(0);
     });
   });
-
   // 以下に sum, factorial のテストを記載せよ
+
+  describe("sum", () => {
+    it("returns sum for elements in an array", () => {
+      expect(sum([1, 2, 3, 4])).toBe(10);
+    });
+
+    it("returns sum for elements in an array", () => {
+      expect(sum([-1, -2, -3, -4])).toBe(-10);
+    });
+  });
+
+  describe("factorial", () => {
+    it("returns factorial value for a number", () => {
+      expect(factorial(5)).toBe(120);
+    });
+
+    it("returns factorial value for a number", () => {
+      expect(factorial(100)).toBe(9.332621544394418e+157);
+    });
+  });
+
 });
