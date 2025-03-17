0bf628ca19a4a3bcaa8a1620b15e2d88593308c4
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-03-17 20:56:41 +0900

    ch02/ex04-05

---
+++ ch02/ex03/index.js
@@ -0,0 +1,11 @@
+// "\u{30d1}ン"
+console.log("NFCバージョン：");
+console.log("\u{30d1}ン");
+
+// 変換サイト　https://tech-unlimited.com/escape-unicode.html
+
+// "ハ\u{309A}ン"
+console.log("NFDバージョン：");
+console.log("\u30cf\u{309A}ン");
+
+// 濁音はu\309A、半濁音はu\3099
---
+++ ch02/ex03/index.md
@@ -0,0 +1,8 @@
+## Windows
+NFC
+
+## macOS
+NFD
+
+
+source: https://www.yujikudo.com/post/9837#google_vignette
