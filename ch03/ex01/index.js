375f12b9053ac8b51b6de3599aa0c18a35640afa
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-03-24 21:27:35 +0900

    ch03/ex01-02

---
+++ ch03/ex01/index.js
@@ -0,0 +1,51 @@
+// 正負の  Infinity と NaN で +, -, \*, / の計算を全ての組み合わせでして結果を見なさい。
+
+let a = Infinity;
+let b = -Infinity;
+let c = NaN;
+
+console.log("plus=======================")
+console.log("Infinity, Infinity: ", a + a);
+console.log("Infinity, -Infinity: ", a + b);
+console.log("Infinity, NaN: ", a + c);
+console.log("-Infinity, Infinity: ", b + a);
+console.log("-Infinity, -Infinity: ", b + b);
+console.log("-Infinity, NaN: ", b + c);
+console.log("NaN, Infinity: ", c + a);
+console.log("NaN, -Infinity: ", c + b);
+console.log("NaN, NaN: ", c + c);
+
+
+console.log("minus=======================")
+console.log("Infinity, Infinity: ", a - a);
+console.log("Infinity, -Infinity: ", a - b);
+console.log("Infinity, NaN: ", a - c);
+console.log("-Infinity, Infinity: ", b - a);
+console.log("-Infinity, -Infinity: ", b - b);
+console.log("-Infinity, NaN: ", b - c);
+console.log("NaN, Infinity: ", c - a);
+console.log("NaN, -Infinity: ", c - b);
+console.log("NaN, NaN: ", c - c);
+
+console.log("multiply=======================")
+console.log("Infinity, Infinity: ", a * a);
+console.log("Infinity, -Infinity: ", a * b);
+console.log("Infinity, NaN: ", a * c);
+console.log("-Infinity, Infinity: ", b * a);
+console.log("-Infinity, -Infinity: ", b * b);
+console.log("-Infinity, NaN: ", b * c);
+console.log("NaN, Infinity: ", c * a);
+console.log("NaN, -Infinity: ", c * b);
+console.log("NaN, NaN: ", c * c);
+
+console.log("devide=======================")
+console.log("Infinity, Infinity: ", a / a);
+console.log("Infinity, -Infinity: ", a / b);
+console.log("Infinity, NaN: ", a / c);
+console.log("-Infinity, Infinity: ", b / a);
+console.log("-Infinity, -Infinity: ", b / b);
+console.log("-Infinity, NaN: ", b / c);
+console.log("NaN, Infinity: ", c / a);
+console.log("NaN, -Infinity: ", c / b);
+console.log("NaN, NaN: ", c / c);
+
---
+++ ch03/ex02/index.js
@@ -0,0 +1,9 @@
+console.log("最大値： ", Number.MAX_VALUE);
+console.log("最小値： ", Number.MIN_VALUE);
+
+console.log("最大値＋１： ", Number.MAX_VALUE + 1);
+
+console.log("最大値＋１と最大値＋２の比較： ", (Number.MAX_VALUE + 1) === (Number.MAX_VALUE + 2));
+
+// => true 
+// 表現可能な桁より下の数の加算のため無視されて両方とも最大値のままとなるため 
