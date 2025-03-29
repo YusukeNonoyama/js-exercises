6c6935e34ffaaf397fd243632d68e23fcac5eb13
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-03-29 20:46:08 +0900

    ch03/ex06

--- ch03/ex06/index.js
+++ ch03/ex06/index.js
@@ -19,22 +19,66 @@ export function substring(str, indexStart, indexEnd=str.length) {
   return substr;
 }
 
-export function slice(str, indexStart, indexEnd) {
-  // TODO: ここを実装しなさい
-  return "TODO";
+export function slice(str, indexStart=0, indexEnd=str.length) {
+  let substr = "";
+  if (isNaN(indexStart)){
+    indexStart = 0;
+  }
+  if (isNaN(indexEnd)){
+    indexEnd = 0;
+  }
+  if (indexStart < 0){
+    indexStart = str.length + indexStart;
+  }
+  if (indexEnd < 0){
+    indexEnd = str.length + indexEnd;
+  }
+  if (indexStart > indexEnd){
+    return "";
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (i >= Math.floor(indexStart) && i < Math.floor(indexEnd)){
+      substr += str[i];
+    }
+  }
+  return substr;
 }
 
-export function padStart(str, targetLength, padString) {
-  // TODO: ここを実装しなさい
-  return "TODO";
+export function padStart(str, targetLength, padString=" ") {
+  let substr = "";
+  if ( targetLength < str.length){
+    return str;
+  }
+  let paddingLength = targetLength - str.length;
+
+  for (let i = 0; i < paddingLength; i++){
+    substr += padString[i % padString.length];
+  }
+  substr += str;
+  return substr;
 }
 
 export function trim(str) {
-  // TODO: ここを実装しなさい
-  return "TODO";
+  let substr = ""
+  let trimStart;
+  let trimEnd;
+  for (let i = 0; i < str.length; i++){
+    if (str[i] !== " "){
+      trimStart = i;
+      break
+    }
+  }
+  for (let i = str.length - 1; i >= 0; i--){
+    if (str[i] !== " "){
+      trimEnd = i;
+      break
+    }
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (i >= trimStart && i <= trimEnd){
+      substr += str[i];
+    }
+  }
+  return substr;
 }
 
-// console.log(substring("test", 1, 3));
-// console.log(substring("test", 1));
-console.log("Hello World!".substring(2, NaN));
-console.log(substring("Hello World!", 2, NaN));
