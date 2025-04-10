5b7e3ea138f2241c1febe13c27108d667947b2eb
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-04-10 20:11:59 +0900

    ch03/ex07 問題変更対応

--- ch03/ex07/index.js
+++ ch03/ex07/index.js
@@ -1,18 +1,29 @@
 export function equalArrays(a, b) {
-    // メモ：　参照先が明らかに違う値になると参照が切れているのでここでtrueにするのは不可能に見える
-    if (a === b) return true;                    // 同一の配列は等しい
-    // length: メソッドを持つ型
-    if (a.length !== b.length) return false;     // 長さが異なる配列は等しくない
+    if (a === b) return true;
+    if (a.length !== b.length) return false;
     for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) return false;         // 1つでも違っていれば等しくない
+      if (a[i] !== b[i]) return false;
     }
-    return true;                                // すべて同じであれば等しい
-}
+    return true;
+  }
+  
 
-console.log(equalArrays(8, 888))    // 数値に.lengthは定義されていないからundefinedが返る
-console.log(equalArrays(true, false))    // 論理型に.lengthは定義されていないからundefinedが返る
-console.log(equalArrays({x: 2}, {x: 888, y: 8888}))    // オブジェクトに.lengthは定義されていないからundefinedが返る
-console.log(equalArrays(8, {x: 888, y: 8888}))
-console.log(equalArrays(true, {x: 2}))
-console.log(equalArrays([1, 0], [1, -0]))   // 明らかに違う値ではないけど一応
-console.log(equalArrays([1, " "], [1, ` `]))    // 明らかに違う値ではないけど一応
+
+// export function equalArrays(a, b) {
+//     // メモ：　参照先が明らかに違う値になると参照が切れているのでここでtrueにするのは不可能に見える
+//     if (a === b) return true;                    // 同一の配列は等しい
+//     // length: メソッドを持つ型
+//     if (a.length !== b.length) return false;     // 長さが異なる配列は等しくない
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] !== b[i]) return false;         // 1つでも違っていれば等しくない
+//     }
+//     return true;                                // すべて同じであれば等しい
+// }
+
+// console.log(equalArrays(8, 888))    // 数値に.lengthは定義されていないからundefinedが返る
+// console.log(equalArrays(true, false))    // 論理型に.lengthは定義されていないからundefinedが返る
+// console.log(equalArrays({x: 2}, {x: 888, y: 8888}))    // オブジェクトに.lengthは定義されていないからundefinedが返る
+// console.log(equalArrays(8, {x: 888, y: 8888}))
+// console.log(equalArrays(true, {x: 2}))
+// console.log(equalArrays([1, 0], [1, -0]))   // 明らかに違う値ではないけど一応
+// console.log(equalArrays([1, " "], [1, ` `]))    // 明らかに違う値ではないけど一応
--- ch03/ex07/index.test.js
+++ ch03/ex07/index.test.js
@@ -1,8 +1,19 @@
-import { equalArrays } from "./index.js"; // typescript で書く場合は "./index.ts"
+import { equalArrays } from "./index.js";
 
-describe("equalArrays", () => {
-    it("明らかに違う値だがtrueを返す引数", () => {
-        expect(equalArrays([0, 1], [-0, 1])).toBe(true);
-        expect(equalArrays([1, " "], [1, ` `])).toBe(true);
-    });
+test("ch03-ex07", () => {
+  const x = true; // ここを変更
+  const y = false; // ここを変更
+
+  expect(equalArrays(x, y)).toBe(true);
+  expect(x).not.toEqual(y);
 });
+
+test("ch03-ex07", () => {
+    const x = {x: 2}; // ここを変更
+    const y = {x: 888, y: 8888}; // ここを変更
+  
+    expect(equalArrays(x, y)).toBe(true);
+    expect(x).not.toEqual(y);
+  });
+
+// lengthが定義されていない者同士を引数にすると全てtrueとなる
