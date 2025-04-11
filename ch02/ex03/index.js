892b4510b4408591ae2c44f5ed8458b8ff90618b
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-04-11 19:09:17 +0900

    ch02/03修正

--- ch02/ex03/index.js
+++ ch02/ex03/index.js
@@ -1,11 +1,9 @@
 // "\u{30d1}ン"
-console.log("NFCバージョン：");
-console.log("\u{30d1}ン");
+console.log("NFCバージョン：", "\u{30d1}ン");
 
 // 変換サイト　https://tech-unlimited.com/escape-unicode.html
 
 // "ハ\u{309A}ン"
-console.log("NFDバージョン：");
-console.log("\u30cf\u{309A}ン");
+console.log("NFDバージョン：", "\u30cf\u{309A}ン");
 
 // 濁音はu\309A、半濁音はu\3099
