59a41a6c908ddf77dc14734923628a154f39d6f4
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-04-11 19:52:45 +0900

    ch03/ex06修正

--- ch03/ex06/index.js
+++ ch03/ex06/index.js
@@ -1,21 +1,16 @@
 export function slice(str, indexStart=0, indexEnd=str.length) {
   let substr = "";
-  if (isNaN(indexStart)){
-    indexStart = 0;
-  }
-  if (isNaN(indexEnd)){
-    indexEnd = 0;
-  }
-  if (indexStart < 0){
-    indexStart = str.length + indexStart;
-  }
-  if (indexEnd < 0){
-    indexEnd = str.length + indexEnd;
-  }
-  if (indexStart > indexEnd){
-    return "";
-  }
+  // NaNは0として扱われる
+  if (isNaN(indexStart)) indexStart = 0;
+  if (isNaN(indexEnd)) indexEnd = 0;
+  // 負の場合は後ろから数える
+  if (indexStart < 0) indexStart = str.length + indexStart;
+  if (indexEnd < 0) indexEnd = str.length + indexEnd;
+  // startの方が大きい場合は空文字を返す
+  if (indexStart > indexEnd) return "";
+  //  文字数分をループ
   for (let i = 0; i < str.length; i++) {
+    // indexStartとindexEndの間を出力、Math.floorは小数点への対応
     if (i >= Math.floor(indexStart) && i < Math.floor(indexEnd)){
       substr += str[i];
     }
