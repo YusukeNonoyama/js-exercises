e13a96cc2eb0f86da36a687227098c9bffdb18a0
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-04-11 19:34:26 +0900

    ch03/ex05修正

--- ch03/ex05/index.js
+++ ch03/ex05/index.js
@@ -5,8 +5,3 @@ export function crlfToLf(input) {
 export function lfToCrlf(input) {
     return input.replaceAll("\n", "\r\n");
 }
-
-let input_string = "This is a test sentence.\nThis is the second line."
-
-console.log(input_string);
-console.log(crlfToLf(input_string));
--- ch03/ex05/index.test.js
+++ ch03/ex05/index.test.js
@@ -1,14 +1,14 @@
 import { crlfToLf, lfToCrlf } from "./index.js";
 
-describe("crlf/lf conversion", () => {
-    describe("crlf_to_lf", () => {
+describe("EOL conversion", () => {
+    describe("crlf to lf", () => {
         it("check if conversion CRLF to LF is correctly implemented", () => {
             let input = "This is the first line.\r\nThis is the second line."
             expect(crlfToLf(input).includes("\r\n")).toBe(false);
         });
     });
 
-    describe("lf_to_crlf", () => {
+    describe("lf to crlf", () => {
         it("check if conversion LF to CRLF is correctly implemented", () => {
             let input = "This is the first line.\nThis is the second line."
             expect(lfToCrlf(input).includes("\r\n")).toBe(true);
