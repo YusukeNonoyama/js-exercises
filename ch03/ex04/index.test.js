6b68b7dab7b68c69dd7dceb2fcf111c592458742
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-04-11 19:29:08 +0900

    ch03/ex04修正

--- ch03/ex04/index.test.js
+++ ch03/ex04/index.test.js
@@ -1,38 +1,8 @@
-// import { is_same_value } from "./index.js";
-
 describe("", () => {
+    it("check the length of 'Hundred Points Symbol'", () => {        
+        expect("💯".length).toBe(2);
+    });
     it("check if two values are the same", () => {        
-
-        console.log("💯".length)
-        // console.log("💯")
-        // console.log("\uD83D\uDCAF")
-        // console.log("\u{0001F4AF}")
-
         expect("\uD83D\uDCAF".length === "\u{0001F4AF}".length).toBe(true);
     });
 });
-
-// import { Point } from "./index.js";
-
-// describe("math", () => {
-//   describe("add", () => {
-//     it("add coordinates to Point", () => {
-//       let point = new Point(1, 1)
-//       point.add(2,3);
-//       expect(point.x).toBe(3);
-//       expect(point.y).toBe(4);
-//     });
-//     it("add coordinates to Point", () => {
-//       let point = new Point(100, 100)
-//       point.add(-23,-44);
-//       expect(point.x).toBe(77);
-//       expect(point.y).toBe(56);
-//     });
-//     it("add coordinates to Point", () => {
-//       let point = new Point(1, 1)
-//       point.add(2,3);
-//       expect(point.x).toBe(3);
-//       expect(point.y).toBe(4);
-//     });
-//   });
-// });
