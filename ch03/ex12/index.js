d244a1fc3acfc9c19fc81611accfdb7077c576e8
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-04-12 21:00:40 +0900

    ch03ex13修正

--- ch03/ex12/index.js
+++ ch03/ex12/index.js
@@ -1,17 +1,17 @@
 class Example  {
     valueOf() {
-        console.log(this);
-        // if (typeof(this) === "number" ) return "valueOf() is called";
-        // return {};
-        return {};
+        console.log("valueOf() is called");
+        return 200;
     }
 
     toString() {
-        return "toString() is called";
+        console.log("toString() is called");
+        return "200";
     }
 }
 
 let obj = new Example();
 
-console.log(+obj);
-console.log(obj + " ");
+console.log(+obj);  // valueOf()を呼び出す
+console.log(`${obj}`);  // テンプレートリテラルでtoString()を呼び出す
+// console.log(String(obj));   // これはほぼ直接呼んでいる
