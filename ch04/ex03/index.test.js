9342ad656125cc02fb3989bbfc99904739e610df
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-04-13 21:43:44 +0900

    ch04/ex03

---
+++ ch04/ex03/index.test.js
@@ -0,0 +1,19 @@
+import { sub } from "./index.js"; // typescript で書く場合は "./index.ts"
+
+describe("bit wise subtraction", () => {
+    it("case1", () => {
+        expect(sub(8, 3)).toBe(5);
+    });
+    it("case2", () => {
+        expect(sub(10304, 123)).toBe(10181);
+    });
+    it("case3", () => {
+        expect(sub(15, -3)).toBe(18);
+    });
+    it("case4", () => {
+        expect(sub(8, 100)).toBe(-92);
+    });
+    it("case5", () => {
+        expect(sub(-21, 100)).toBe(-121);
+    });
+});
