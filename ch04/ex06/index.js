724422703e6cdb8029bcad0437f1f722695f9b92
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-04-14 17:14:31 +0900

    ch04/ex06

---
+++ ch04/ex06/index.js
@@ -0,0 +1,52 @@
+// 何らかのリサイズを行う関数と思って読んで下さい
+//
+// - params には undefined またはオブジェクトが与えられる
+// - params.maxWidth が与えられる場合 (正の整数と仮定して良い) はその値を利用する
+// - params.maxHeight が与えられる場合 (正の整数と仮定して良い) はその値を利用する
+function resize(params) {
+    let maxWidth = 600;
+    let maxHeight = 480;
+
+    if (params && params.maxWidth) {
+        maxWidth = params.maxWidth;
+    }
+
+    if (params && params.maxHeight) {
+        maxHeight = params.maxHeight;
+    }
+
+    console.log({ maxWidth, maxHeight });
+}
+
+
+function resize1(params) {
+    let maxWidth = 600;
+    let maxHeight = 480;
+
+    // paramsがundefinedでなければparamsのプロパティを呼び出す。paramsがundefinedの場合は最後のmaxWidth/maxHeightとなる。
+    maxWidth = params && params.maxWidth || maxWidth;
+    maxHeight = params && params.maxHeight || maxHeight;
+
+    console.log({ maxWidth, maxHeight });
+}
+
+
+function resize2(params) {
+    let maxWidth = 600;
+    let maxHeight = 480;
+
+    // .?はプロパティがない場合(nullとundefined)にundefinedを返す。??は左の値を採用するが、左の値がnullかundefinedの場合は右の値を採用する。
+    maxWidth = params?.maxWidth ?? maxWidth;
+    maxHeight = params?.maxHeight ?? maxHeight;
+
+    console.log({ maxWidth, maxHeight });
+}
+
+resize({ maxWidth: 200, maxHeight: 150 });
+resize1({ maxWidth: 200, maxHeight: 150 });
+resize2({ maxWidth: 200, maxHeight: 150 });
+resize(undefined);
+resize1(undefined);
+resize2(undefined);
+
+console.log(null?.test);
