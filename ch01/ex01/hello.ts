4e46619e340d6f6d071348d82917302f6e303873
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-04-14 21:56:06 +0900

    typescript update

--- ch01/ex01/hello.ts
+++ ch01/ex01/hello.ts
@@ -1,1 +1,5 @@
 console.log("Hello, World!")
+
+export function func1(){
+    return 200;
+}
---
+++ ch01/ex05/index.spec.ts
@@ -0,0 +1,43 @@
+// import { abs, sum, factorial } from "./index.ts";
+import { abs } from "./index.ts";
+
+// TypeScript の場合は以下:
+// import { abs, sum, factorial } from "./index.ts";
+
+describe("math", () => {
+  describe("abs", () => {
+    it("returns same value when positive value given", () => {
+      expect(abs(42)).toBe(42);
+    });
+
+    it("returns negated value when negative value given", () => {
+      expect(abs(-42)).toBe(42);
+    });
+
+    it("returns zero value when zero given", () => {
+      expect(abs(0)).toBe(0);
+    });
+  });
+  // 以下に sum, factorial のテストを記載せよ
+
+  // describe("sum", () => {
+  //   it("returns sum for elements in an array", () => {
+  //     expect(sum([1, 2, 3, 4])).toBe(10);
+  //   });
+
+  //   it("returns sum for elements in an array", () => {
+  //     expect(sum([-1, -2, -3, -4])).toBe(-10);
+  //   });
+  // });
+
+  // describe("factorial", () => {
+  //   it("returns factorial value for a number", () => {
+  //     expect(factorial(5)).toBe(120);
+  //   });
+
+  //   it("returns factorial value for a number", () => {
+  //     expect(factorial(100)).toBe(9.332621544394418e+157);
+  //   });
+  // });
+
+});
