aaaf4e292c1ca41c34b84dd19a0339878991b963
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-05-10 21:27:27 +0900

    ch04/ex01修正

--- ch04/ex01/index.test.js
+++ ch04/ex01/index.test.js
@@ -6,20 +6,16 @@ describe("complex number calculation", () => {
         let z2 = { real: 5, im: 2 };
 
         it("add", () => {
-            expect(add(z1, z2).real).toBe(6);
-            expect(add(z1, z2).im).toBe(4);
+            expect(JSON.stringify(add(z1, z2))).toBe(JSON.stringify({ real: "6.00", im: "4.00" }));
         });
         it("sub", () => {
-            expect(sub(z1, z2).real).toBe(-4);
-            expect(sub(z1, z2).im).toBe(0);
+            expect(JSON.stringify(sub(z1, z2))).toBe(JSON.stringify({ real: "-4.00", im: "0.00" }));
         });
         it("mul", () => {
-            expect(mul(z1, z2).real).toBe(1);
-            expect(mul(z1, z2).im).toBe(12);
+            expect(JSON.stringify(mul(z1, z2))).toBe(JSON.stringify({ real: "1.00", im: "12.00" }));
         });
         it("div", () => {
-            expect(div(z1, z2).real).toBe(0.31);
-            expect(div(z1, z2).im).toBe(0.28);
+            expect(JSON.stringify(div(z1, z2))).toBe(JSON.stringify({ real: "0.31", im: "0.28" }));
         });
     });
     describe("case2", () => {
@@ -27,20 +23,41 @@ describe("complex number calculation", () => {
         let z2 = { real: -5, im: 0.4 };
 
         it("add", () => {
-            expect(add(z1, z2).real).toBe(-3.7);
-            expect(add(z1, z2).im).toBe(-1.6);
+            expect(JSON.stringify(add(z1, z2))).toBe(JSON.stringify({ real: "-3.70", im: "-1.60" }));
         });
         it("sub", () => {
-            expect(sub(z1, z2).real).toBe(6.3);
-            expect(sub(z1, z2).im).toBe(-2.4);
+            expect(JSON.stringify(sub(z1, z2))).toBe(JSON.stringify({ real: "6.30", im: "-2.40" }));
         });
         it("mul", () => {
-            expect(mul(z1, z2).real).toBe(-5.7);
-            expect(mul(z1, z2).im).toBe(10.52);
+            expect(JSON.stringify(mul(z1, z2))).toBe(JSON.stringify({ real: "-5.70", im: "10.52" }));
         });
         it("div", () => {
-            expect(div(z1, z2).real).toBe(-0.29);
-            expect(div(z1, z2).im).toBe(0.38);
+            expect(JSON.stringify(div(z1, z2))).toBe(JSON.stringify({ real: "-0.29", im: "0.38" }));
+        });
+    });
+    describe("case3: invalid input", () => {
+        let z1 = { real: 1, im: 2 };
+        let z2 = { real: 5, im: 2 };
+
+        it("add", () => {
+            expect(add(z1, 2)).toBe("invalid input: number");
+            expect(add(z1, null)).toBe("invalid input: null");
+            expect(add(z1, undefined)).toBe("invalid input: undefined");
+        });
+        it("sub", () => {
+            expect(sub(z1, 2)).toBe("invalid input: number");
+            expect(sub(z1, null)).toBe("invalid input: null");
+            expect(sub(z1, undefined)).toBe("invalid input: undefined");
+        });
+        it("mul", () => {
+            expect(mul(z1, 2)).toBe("invalid input: number");
+            expect(mul(z1, null)).toBe("invalid input: null");
+            expect(mul(z1, undefined)).toBe("invalid input: undefined");
+        });
+        it("div", () => {
+            expect(div(z1, 2)).toBe("invalid input: number");
+            expect(div(z1, null)).toBe("invalid input: null");
+            expect(div(z1, undefined)).toBe("invalid input: undefined");
         });
     });
 });
