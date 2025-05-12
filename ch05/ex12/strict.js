0ae4f84cf198ae0851523b01d326ab3920d3ab9e
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-05-12 15:26:27 +0900

    ch05修正

--- ch05/ex12/strict.js
+++ ch05/ex12/strict.js
@@ -21,3 +21,8 @@ console.log(JSON.stringify({ a, b, obj }));
 //     at ModuleLoader.loadAndTranslate (node:internal/modules/esm/loader:478:27)
 //     at async ModuleJob._link (node:internal/modules/esm/module_job:110:19)
 // Node.js v20.19.0
+
+
+// ブラウザで実行
+// 実行結果
+// {"a":1,"b":2,"obj":{"a":4,"b":4}}
