0f1a36c09282d98e00300ebea41f219549862d39
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-05-18 16:51:35 +0900

    ch08/ex02修正

--- ch08/ex02/index.ts
+++ ch08/ex02/index.ts
@@ -7,23 +7,24 @@ export function calcExponentialRecursive(x: number, n: number): number {
     if (n === 0) {
         return 1;
     }
+    function multiplyAndCount(x: number, n: number, acc: number) {
+        if (n === 1) {
+            // カウントnが1になったらaccにxをかけて返す（accは余り分の積の値、xは累乗を再帰回数分繰り返した値）
+            return acc * x;
+        }
+        if (n % 2 === 0) {
+            // 奇数の場合はx→x*x, n→n/2として再帰回数を減らす
+            return multiplyAndCount(x * x, Math.floor(n / 2), acc);
+        } else {
+            // 偶数の場合は奇数と同様にした上で、余りの１回分をacc*xとして返す
+            return multiplyAndCount(x * x, Math.floor(n / 2), acc * x);
+        }
+    }
     // accにxを１回掛けたらカウントnを減らす末尾再帰関数
     return multiplyAndCount(x, n, 1);
 }
 
-function multiplyAndCount(x: number, n: number, acc: number) {
-    if (n === 1) {
-        // カウントnが1になったらaccにxをかけて返す（accは余り分の積の値、xは累乗を再帰回数分繰り返した値）
-        return acc * x;
-    }
-    if (n % 2 === 0) {
-        // 奇数の場合はx→x*x, n→n/2として再帰回数を減らす
-        return multiplyAndCount(x * x, Math.floor(n / 2), acc);
-    } else {
-        // 偶数の場合は奇数と同様にした上で、余りの１回分をacc*xとして返す
-        return multiplyAndCount(x * x, Math.floor(n / 2), acc * x);
-    }
-}
+
 
 
 // ループ削減バージョン
