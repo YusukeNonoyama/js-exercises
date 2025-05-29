3a7ba82959252d7fe16dac5d67cbfa9ea4afbe84
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-05-30 07:11:05 +0900

    ch09/ex07

--- ch09/ex07/index.test.js
+++ ch09/ex07/index.test.js
@@ -12,3 +12,21 @@ describe("InstrumentedLinkedList", () => {
     expect(list.pushCount).toBe(2);
   });
 });
+
+// 結果：
+
+// npm run test:js ./ch09/ex07/
+
+// > preset-ts@1.0.0 test:js
+// > node --experimental-vm-modules node_modules/jest/bin/jest.js --runInBand ./ch09/ex07/
+
+//  PASS  ch09/ex07/index.test.js
+//   InstrumentedLinkedList
+//     ✓ #push (1 ms)
+//     ✓ #pushAll
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.659 s, estimated 1 s
+// Ran all test suites matching /.\/ch09\/ex07\//i.
