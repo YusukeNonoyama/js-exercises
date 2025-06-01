ba3195c029d093579f861f8765f58d29e175c8c2
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-01 18:55:12 +0900

    ch10/ex03

---
+++ ch10/ex03/index.cjs
@@ -0,0 +1,4 @@
+const { foo, Bar } = require("./module.cjs");
+
+console.log(foo());
+console.log(new Bar().baz());
---
+++ ch10/ex03/module.cjs
@@ -0,0 +1,9 @@
+exports.foo = function(){
+    return "Function from module.cjs"
+}
+
+exports.Bar = class {
+    baz(){
+        return "Class method from module.cjs"
+    };
+}
