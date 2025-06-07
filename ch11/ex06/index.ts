e4d16c9ade0a0fa8ac42fb554a8fcf8eca46afb0
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-07 21:15:08 +0900

    ch11/ex06

--- ch11/ex06/index.ts
+++ ch11/ex06/index.ts
@@ -2,7 +2,7 @@ export function isEmailAddress(input: string){
     if(!input){
         return false;
     }
-    const pattern = /^(\w+\.\w+|(\w|[!#$%&'*+\-/=?^_`{|}~]){1,64})@((\w|[!#$%&'*+\-/=?^_`{|}~])+\.(\w|[!#$%&'*+\-/=?^_`{|}~])+|(\w){1,252})$/u;
+    const pattern = /^(\w+\.\w+|(\w|[!#$%&'*+\-/=?^_`{|}~]){1,64})@((\w|[!#$%&'*+\-/=?^_`{|}~])+\.(\w|[!#$%&'*+\-/=?^_`{|}~])+|\w{1,252})$/u;
         if(input.match(pattern)){
             return true;
         }
