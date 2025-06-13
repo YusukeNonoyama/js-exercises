bf0c83201be19a12cd23f15c2a2ce736e7296807
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-13 19:28:16 +0900

    ch06/ex02

--- ch06/ex02/index.ts
+++ ch06/ex02/index.ts
@@ -1,5 +1,5 @@
-let o = {x: 1, y: 2};
-let o2 = Object.create(o);
+let o1 = {x: 1, y: 2};
+let o2 = Object.create(o1);
 
 console.log(o2);    // {}
 console.log(Object.getPrototypeOf(o2)); // { x: 1, y: 2 }
