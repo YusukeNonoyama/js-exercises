6361c94608ad3b23cd23e215185ec490fc6a6196
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-16 22:00:37 +0900

    ch07修正

--- ch07/ex05/index.ts
+++ ch07/ex05/index.ts
@@ -18,14 +18,3 @@ export function unshift(arr: number[], value: number) {
 export function sort(arr: number[], callback: (a: number, b: number) => number) {
     return [...arr].sort(callback);
 }
-
-// const seq = [1, 2, 3, 4, 5];
-
-// console.log(pop(seq)); // [1, 2, 3, 4]
-// console.log(push(seq, 6)); // [1, 2, 3, 4, 5, 6]
-// console.log(shift(seq)); // [2, 3, 4, 5]
-// console.log(unshift(seq, 0)); // [0, 1, 2, 3, 4, 5]
-// console.log(sort(seq, (a, b) => b - a)); // [5, 4, 3, 2, 1]
-
-// 元の配列は変更されていない
-// console.log(seq); // [1, 2, 3, 4, 5]
