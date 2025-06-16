99fbd89b0ddde010dd0cf52258d8b794e9511da6
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-16 21:56:21 +0900

    ch07修正

--- ch07/ex07/index.ts
+++ ch07/ex07/index.ts
@@ -3,11 +3,9 @@ export function bubbleSort(arr: number[]) {
     if(!Array.isArray(arr)) {
         throw Error(`invalid input: ${arr}`)
     }
-    // return "invalid input";
     if(!arr.every(x => typeof x === "number")){
         throw Error(`invalid array element: ${arr}`)
     }
-        // return "invalid array element";
     const n = arr.length;
     for (let i = 0; i < n - 1; i++) {
         // n-1-i番目以降はソート済みなので、未ソート箇所を対象に繰り返す
@@ -20,35 +18,3 @@ export function bubbleSort(arr: number[]) {
     }
     return [...arr];
 }
-
-const a = [8, 4, 7, 2, 1, 3, 5, 6, 9, 10, 5];
-const b = [8, 4, 7, 2, 1, 3, 5, 6, 9, "b", 5];
-console.log(a);
-bubbleSort(a);
-console.log(a);
-
-console.log(b.every(x => typeof x === "number"));
-
-// function sort(
-//   array,
-//   compare = (lhs, rhs) => (lhs < rhs ? -1 : lhs > rhs ? +1 : 0)
-// ) {
-//   // array[0 ... i-1] が常にソート済みになるように処理を進める
-//   // (0 <= j < i-1 に対して compare(array[j], array[j + 1]) <= 0 が成り立つ)
-//   for (let i = 1; i < array.length; i++) {
-//     const v = array[i];
-
-//     // array[i] を array[0 ... i] の適切な場所に挿入する
-//     let j = i;
-//     while (j > 0 && compare(array[j - 1], v) > 0) {
-//       array[j] = array[j - 1];
-//       j--;
-//     }
-//     array[j] = v;
-//   }
-//   return array;
-// }
-
-// let a = [2, 4, 5, 1, 6, 2];
-
-// console.log(sort(a));
