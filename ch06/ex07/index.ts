52e5df95a0ffa7d1e06ecbe8fb3de42aed3c2df3
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-17 21:06:06 +0900

    ch06/ex07

--- ch06/ex07/index.ts
+++ ch06/ex07/index.ts
@@ -1,13 +1,21 @@
 export function assign(input: any, ...output: any){
-    // inputがnullはエラー
-    Object.keys(input);
-    // NumberのWrapperオブジェクトを作成
-    if (typeof input === "number") input = new Number(input);
-    if (typeof input === "boolean") input = new Boolean(input);
+    if(input === null|| input === undefined){
+        throw new TypeError("Cannot convert undefined or null to object");
+    }
+    // Wrapperオブジェクトを作成 
+    // Number {..}, Boolean{..}という形式（assign()が期待する返り値）
+    if (typeof input === "number") {
+        input = new Number(input)
+    };
+    if (typeof input === "boolean") {
+        input = new Boolean(input)
+    };
     let result_o = input;
     for (let o of output){
         // Arrayのままだとlengthプロパティが入るのindexをプロパティにしたオブジェクトに変換
-        if (Array.isArray(o)) o = Object.fromEntries(o.entries());  
+        if (Array.isArray(o)) {
+            o = Object.fromEntries(o.entries())
+        };  
         if (typeof o !== "object" || o === null) continue;
         // Symbol以外の独自プロパティを追加
         for (const p of Object.getOwnPropertyNames(o)){
@@ -21,46 +29,3 @@ export function assign(input: any, ...output: any){
     }
     return result_o;
 }
-
-// let input = { foo: "foo", hello: "world" }
-// let output = [
-//     { foo: "fooo", bar: "bar" },
-//     { foo: "foooo", fizz: "fizz", buzz: "buzz" },
-//     123,
-// ]
-// let input = { foo: "foo", hello: "world" };
-// let output = [
-//     123,
-//     true,
-//     ["aa", "bb", "cc"],
-//     null,
-//     undefined,
-//   ];
-
-// let input = 1
-// let output = [{ foo: "foo", bar: "bar" }]
-
-// let input = true
-// let output = [{ foo: "foo", bar: "bar" }]
-
-// let input = null
-// let output = [{ foo: "foo", bar: "bar" }]
-
-
-// console.log(assign(input, ...output));
-// console.log(Object.assign(input, ...output));
-
-
-// const arr = ["aa", "bb", "cc"];
-// const oFromArray = Object.fromEntries(arr.entries())
-// console.log(oFromArray);
-
-// console.log(Object.getOwnPropertyNames(123));
-
-// const n = new Number(1);  // wrapper object, not primitive
-// n.foo = 'foo';
-// n.bar = 'bar';
-
-// console.log(n); // [Number: 1] { foo: 'foo', bar: 'bar' }
-
-// Object.keys(undefined);
