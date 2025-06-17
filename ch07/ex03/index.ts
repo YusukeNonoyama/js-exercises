bbe228eb58136ce2fcfa99e5b6d39c707f517a62
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-17 22:04:52 +0900

    ch07/ex03

--- ch07/ex03/index.ts
+++ ch07/ex03/index.ts
@@ -1,15 +1,12 @@
 export function sum(array: number[]) {
-  // 引数がarrayでないとreduceが呼び出せないため入力確認
-  if (typeof array !== "object") return 0;
-  // reduce()で和算
+  if (!Array.isArray(array)) return 0;
   return array.reduce((x, y) => x + y, 0);
 }
 
 export function join(array: (number | string)[], separator = ",") {
-  // 入力配列が空の時、nullの時の挙動を定義
   if (array.length === 0) return "";
   if (separator === null) separator = "null";
-  // arrayの中がnullの場合は空文字に置き換える。ループ前にやらないと要素がスキップされる。
+  // arrayの中がnullの場合は空文字に置き換える
   array = array.map(x => x === null ? "" : x);
   return array.reduce((x, y) => x + separator + y);
 }
