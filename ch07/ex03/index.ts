1be6054418d4985e98eb79fb9fe8e3448c0bca3b
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-18 07:09:10 +0900

    ch07/ex03

--- ch07/ex03/index.ts
+++ ch07/ex03/index.ts
@@ -15,16 +15,21 @@ export function reverse(array: (number | string)[]) {
   return array.reduce((x, y) => [y, ...x], [] as (number | string)[]);
 }
 
+// 全ての配列要素がtrueならtrueを返す、１つでもfalseがあればfalseを返す
 export function every(array: number[], callback: (y: number, index: number, a: number[]) => boolean) {
   return array.reduce((x, y, index, a) => {
-    if (!x) return x;
+    // xがfalseならfalseを返して終了
+    if (!x) return false;
+    // xがtrueならcallbackでyを評価。「結果がfalseなら次のxがfalseになり終了、結果がtrueなら次のyを評価」を繰り返す
+    // テストケースの引数１つと３つの場合に両方対応したcallback
     return callback(y, index, a);
   }, true);
 }
 
+// 全ての配列要素がfalseならfalseを返す、1つでもtrueがあればtrueを返す
 export function some(array: number[], callback: (y: number, index: number, a: number[]) => boolean) {
   return array.reduce((x, y, index, a) => {
-    if (x) return x;
+    if (x) return true;
     return callback(y, index, a);
   }, false);
 }
