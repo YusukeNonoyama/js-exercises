c909bb69cb9c307a5991649b2e936d2346bae0f1
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-17 18:00:05 +0900

    ch06/ex02修正

--- ch06/ex02/index.ts
+++ ch06/ex02/index.ts
@@ -1,5 +1,12 @@
+// 独自プロパティを持つオブジェクト
 let o1 = {x: 1, y: 2};
+// そのオブジェクトをプロトタイプとして持つ新しいオブジェクト
 let o2 = Object.create(o1);
 
 console.log(o2);    // {}
 console.log(Object.getPrototypeOf(o2)); // { x: 1, y: 2 }
+
+console.log(o2.x);  // 1
+console.log(o2.y);  // 2
+
+// プロパティが渡されている
