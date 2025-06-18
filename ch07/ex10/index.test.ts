1fb0567e195f5a7f4614054d21aa6d445a05e7fd
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-06-18 19:53:18 +0900

    ch07/ex10修正

--- ch07/ex10/index.test.ts
+++ ch07/ex10/index.test.ts
@@ -103,3 +103,53 @@ describe("DynamicSizeArray push()", () => {
         expect(a.get(4)).toBe(50);
     });
 });
+
+
+
+// 結果：
+// npm run test:ts ch07/ex10
+
+// > preset-ts@1.0.0 test:ts
+// > jest --runInBand --coverage ch07/ex10
+
+//  PASS  ch07/ex10/index.test.ts
+//   DynamicSizeArray get()
+//     ✓ get(0): 10 (2 ms)
+//     ✓ get(1): undefined
+//     ✓ get(2): 20 (1 ms)
+//     ✓ get(3): 30
+//     ✓ get(-1) error: function Error() { [native code] } (11 ms)
+//     ✓ get(4) error: function Error() { [native code] } (1 ms)
+//     ✓ get(one) error: function Error() { [native code] } (1 ms)
+//     ✓ get(NaN) error: function Error() { [native code] } (1 ms)
+//   DynamicSizeArray set()
+//     ✓ set(0): 10 (1 ms)
+//     ✓ set(1): 20 (1 ms)
+//     ✓ set(2): 30
+//     ✓ set(-1) error: 10 (1 ms)
+//     ✓ set(4) error: 10 (1 ms)
+//     ✓ set(one) error: 10 (1 ms)
+//     ✓ set(NaN) error: 10 (1 ms)
+//     ✓ set() value check (1 ms)
+//     ✓ set() value check
+//     ✓ set() value check (1 ms)
+//   DynamicSizeArray length()
+//     ✓ length(): 4 (1 ms)
+//     ✓ length(): 4 (2 ms)
+//     ✓ length(): 8
+//   DynamicSizeArray push()
+//     ✓ push(): 1
+//     ✓ push(): 4 (1 ms)
+//     ✓ push(): 5 (1 ms)
+
+// ----------|---------|----------|---------|---------|-------------------
+// File      | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s 
+// ----------|---------|----------|---------|---------|-------------------
+// All files |   97.29 |    92.85 |     100 |   97.14 |                   
+//  index.ts |   97.29 |    92.85 |     100 |   97.14 | 12                
+// ----------|---------|----------|---------|---------|-------------------
+// Test Suites: 1 passed, 1 total
+// Tests:       24 passed, 24 total
+// Snapshots:   0 total
+// Time:        2.404 s, estimated 3 s
+// Ran all test suites matching /ch07\/ex10/i.
