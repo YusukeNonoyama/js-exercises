d8f58f466e0ec4cd49aca68261aa4d791786f2c4
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-07-02 21:23:41 +0900

    ch12/ex03

--- ch12/ex03/index.ts
+++ ch12/ex03/index.ts
@@ -10,15 +10,3 @@ export function* counterGen(max: number): Generator<number> {
     }
     console.log("counterGen: finally");
 }
-
-// const gen = counterGen(5);
-
-// console.log(gen.next()); // 1
-// console.log(gen.next()); // 2
-// console.log(gen.next()); // 3
-// console.log(gen.throw("reset")); // 1
-// console.log(gen.next()); // 2
-// console.log(gen.next()); // 3
-// console.log(gen.next()); // 4
-// console.log(gen.next()); // 5
-// console.log(gen.next()); // 終了
