756648b9447bf874750eca9e7d81d66cffb9639d
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-07-10 21:28:50 +0900

    ch08/ex10修正

--- ch08/ex10/index.ts
+++ ch08/ex10/index.ts
@@ -10,3 +10,8 @@ export function addMyCall(f: { myCall: Function, bind: Function }) {
 
 // addMyCall(f);
 // console.log(f.myCall({a: 1}));
+// let g = function () {
+//     return this.x;
+// }
+// const o = { x: 100 };
+// console.log(g.call(o));
