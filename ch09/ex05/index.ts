878f482e2d327d55ec2e3851c931150300e215d7
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-07-14 20:03:19 +0900

    ch09/ex05修正

--- ch09/ex05/index.ts
+++ ch09/ex05/index.ts
@@ -5,40 +5,9 @@ export function instanceOf(object: {}, constructor: Function) {
         }
         // 次のプロトタイプを代入する
         object = Object.getPrototypeOf(object);
-        // object = object.constructor.prototype だとうまくいかない
 
         if (object === null) {
             return false;
         }
     }
 }
-
-// class A {
-//     constructor() { };
-// }
-// class B extends A {
-//     constructor() {
-//         super();
-//     };
-// }
-// class C extends B {
-//     constructor() {
-//         super();
-//     };
-// }
-// class D {
-//     constructor() { };
-// }
-
-// let objectA = new A();
-// let objectB = new B();
-// let objectC = new C();
-// let objectD = new D();
-
-
-// console.log(instanceOf(objectC, A.prototype.constructor));
-// console.log(instanceOf(objectD, A.prototype.constructor));
-// console.log(instanceOf(objectA, A.prototype.constructor));
-
-// console.log(Object.getPrototypeOf(objectC));
-// console.log(objectC.constructor.prototype);
