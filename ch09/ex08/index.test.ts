04f37dc94a208e567435cdee3626d35eb667a23b
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-07-15 19:57:24 +0900

    ch09/ex08守勢

--- ch09/ex08/index.test.ts
+++ ch09/ex08/index.test.ts
@@ -1,64 +1,52 @@
-import { AlarmClock, Normal, AlarmSet, AlarmSounding, Snoozing } from "./index.ts"
+import { AlarmClock, NormalState } from './index.ts';
 
-describe("alarm clock class", () => {
-    test("create alarm clock", () => {
-        let clock = new AlarmClock();
-        expect(clock.getState()).toBe("normal");
+describe('AlarmClock State Pattern', () => {
+    let clock: AlarmClock;
+    beforeEach(() => {
+        clock = new AlarmClock();
     });
-    test('"normal" => "alarmSet"', () => {
-        let clock = new AlarmClock();
-        Normal.setAlarm(clock);
-        expect(clock.getState()).toBe("alarmSet");
-        expect(() => Normal.setAlarm(clock)).toThrow(Error);
+    it('initial NormalState', () => {
+        expect(clock.getState()).toEqual(new NormalState(clock));
     });
-    test('"alarmSet" => "normal"', () => {
-        let clock = new AlarmClock();
-        Normal.setAlarm(clock);
-        AlarmSet.cancelAlarm(clock);
-        expect(clock.getState()).toBe("normal");
-        expect(() => AlarmSet.cancelAlarm(clock)).toThrow(Error);
+    it('NormalState to AlarmSetState', () => {
+        const action = clock.setAlarm();
+        expect(action).toBe("none");
     });
-    test('"alarmSet" => "alarmSounding"', () => {
-        let clock = new AlarmClock();
-        Normal.setAlarm(clock);
-        AlarmSet.reachedToAlarmTime(clock);
-        expect(clock.getState()).toBe("alarmSounding");
-        expect(() => AlarmSet.reachedToAlarmTime(clock)).toThrow(Error);
+    it('AlarmSetState to NormalState', () => {
+        clock.setAlarm();
+        const action = clock.cancelAlarm();
+        expect(action).toBe("none");
+    });
+    it('AlarmSet to AlarmSoundingState', () => {
+        clock.setAlarm();
+        const action = clock.reachedToAlarmTime();
+        expect(action).toBe("soundAlarm");
+    });
 
+    it('AlarmSoundingState to SnoozingState', () => {
+        clock.setAlarm();
+        clock.reachedToAlarmTime();
+        const action = clock.snooze();
+        expect(action).toBe("stopAlarm");
     });
-    test('"alarmSounding" => "snoozing"', () => {
-        let clock = new AlarmClock();
-        Normal.setAlarm(clock);
-        AlarmSet.reachedToAlarmTime(clock);
-        AlarmSounding.snooze(clock);
-        expect(clock.getState()).toBe("snoozing");
-        expect(() => AlarmSounding.snooze(clock)).toThrow(Error);
+    it('AlarmSoundingState to NormalState', () => {
+        clock.setAlarm();
+        clock.reachedToAlarmTime();
+        const action = clock.cancelAlarm();
+        expect(action).toBe("stopAlarm");
     });
-    test('"alarmSounding" => "normal"', () => {
-        let clock = new AlarmClock();
-        Normal.setAlarm(clock);
-        AlarmSet.reachedToAlarmTime(clock);
-        AlarmSounding.cancelAlarm(clock);
-        expect(clock.getState()).toBe("normal");
-        expect(() => AlarmSounding.cancelAlarm(clock)).toThrow(Error);
+    it('SnoozingState to AlarmSoundingState', () => {
+        clock.setAlarm();
+        clock.reachedToAlarmTime();
+        clock.snooze();
+        const action = clock.elapseSnoozeTime();
+        expect(action).toBe("soundAlarm");
     });
-    test('"snoozing" => "alarmSounding"', () => {
-        let clock = new AlarmClock();
-        Normal.setAlarm(clock);
-        AlarmSet.reachedToAlarmTime(clock);
-        AlarmSounding.snooze(clock);
-        Snoozing.elapseSnoozeTime(clock);
-        expect(clock.getState()).toBe("alarmSounding");
-        expect(() => Snoozing.elapseSnoozeTime(clock)).toThrow(Error);
 
-    });
-    test('"snoozing" => "normal"', () => {
-        let clock = new AlarmClock();
-        Normal.setAlarm(clock);
-        AlarmSet.reachedToAlarmTime(clock);
-        AlarmSounding.snooze(clock);
-        Snoozing.cancelAlarm(clock);
-        expect(clock.getState()).toBe("normal");
-        expect(() => Snoozing.cancelAlarm(clock)).toThrow(Error);
+    it('SnoozingState to NormalState', () => {
+        clock.setAlarm();
+        clock.reachedToAlarmTime();
+        const action = clock.cancelAlarm();
+        expect(action).toBe("stopAlarm");
     });
 });
--- ch09/ex08/index.ts
+++ ch09/ex08/index.ts
@@ -1,60 +1,128 @@
-export type State = "normal" | "alarmSet" | "alarmSounding" | "snoozing";
+// Stateパターン
+// Stateパターンでは、「状態」自身が次の遷移先を知っている。
+type Action = "none" | "soundAlarm" | "stopAlarm";
 
-function validateState(clock: AlarmClock, state: State) {
-    if (clock.getState() !== state) {
-        throw Error(`State should be ${state}: ${clock.getState()}`);
-    };
+// 全ての状態が持つメソッドのインターフェースを定義
+interface AlarmState {
+    setAlarm(): Action;
+    cancelAlarm(): Action;
+    reachedToAlarmTime(): Action;
+    snooze(): Action;
+    elapseSnoozeTime(): Action;
 }
 
+// 目覚まし時計クラス
+// インターフェースで定義したメソッドを持ち、各メソッド内で各状態クラスで定義されたメソッドを呼び出す
 export class AlarmClock {
-    #state: State;
+    private state: AlarmState;
     constructor() {
-        this.#state = "normal";
+        this.state = new NormalState(this);
     }
-    getState() {
-        return this.#state;
+    public getState() {
+        return this.state;
     }
-    setState(state: State) {
-        this.#state = state;
+    public setState(state: AlarmState) {
+        this.state = state;
     }
-}
-
-export class Normal {
-    static setAlarm(clock: AlarmClock) {
-        validateState(clock, "normal");
-        clock.setState("alarmSet");
+    public setAlarm(): Action {
+        return this.state.setAlarm();
     }
-}
-
-export class AlarmSet {
-    static cancelAlarm(clock: AlarmClock) {
-        validateState(clock, "alarmSet");
-        clock.setState("normal");
+    public cancelAlarm(): Action {
+        return this.state.cancelAlarm();
     }
-    static reachedToAlarmTime(clock: AlarmClock) {
-        validateState(clock, "alarmSet");
-        clock.setState("alarmSounding");
+    public reachedToAlarmTime(): Action {
+        return this.state.reachedToAlarmTime();
+    }
+    public snooze(): Action {
+        return this.state.snooze();
+    }
+    public elapseSnoozeTime(): Action {
+        return this.state.elapseSnoozeTime();
     }
 }
 
-export class AlarmSounding {
-    static cancelAlarm(clock: AlarmClock) {
-        validateState(clock, "alarmSounding");
-        clock.setState("normal");
+export class NormalState implements AlarmState {
+    constructor(private clock: AlarmClock) { }
+    setAlarm(): Action {
+        // 状態遷移
+        // clockを受け取った状態クラスのインスタンスを渡す
+        this.clock.setState(new AlarmSetState(this.clock));
+        return "none";
     }
-    static snooze(clock: AlarmClock) {
-        validateState(clock, "alarmSounding");
-        clock.setState("snoozing");
+    cancelAlarm(): Action {
+        return "none";
+    }
+    reachedToAlarmTime(): Action {
+        return "none";
+    }
+    snooze(): Action {
+        return "none";
+    }
+    elapseSnoozeTime(): Action {
+        return "none";
     }
 }
 
-export class Snoozing {
-    static cancelAlarm(clock: AlarmClock) {
-        validateState(clock, "snoozing");
-        clock.setState("normal");
+// 以下同様に状態クラスを図に従って定義
+class AlarmSetState implements AlarmState {
+    constructor(private clock: AlarmClock) { }
+    setAlarm(): Action {
+        return "none";
     }
-    static elapseSnoozeTime(clock: AlarmClock) {
-        validateState(clock, "snoozing");
-        clock.setState("alarmSounding");
+    cancelAlarm(): Action {
+        this.clock.setState(new NormalState(this.clock));
+        return "none";
+    }
+    reachedToAlarmTime(): Action {
+        this.clock.setState(new AlarmSoundingState(this.clock));
+        return "soundAlarm";
+    }
+    snooze(): Action {
+        return "none";
+    }
+    elapseSnoozeTime(): Action {
+        return "none";
+    }
+}
+
+class AlarmSoundingState implements AlarmState {
+    constructor(private clock: AlarmClock) { }
+    setAlarm(): Action {
+        return "none";
+    }
+    cancelAlarm(): Action {
+        this.clock.setState(new NormalState(this.clock));
+        return "stopAlarm";
+    }
+    reachedToAlarmTime(): Action {
+        return "none";
+    }
+    snooze(): Action {
+        this.clock.setState(new SnoozingState(this.clock));
+        return "stopAlarm";
+    }
+    elapseSnoozeTime(): Action {
+        return "none";
+    }
+}
+
+class SnoozingState implements AlarmState {
+    constructor(private clock: AlarmClock) { }
+    setAlarm(): Action {
+        return "none";
+    }
+    cancelAlarm(): Action {
+        this.clock.setState(new NormalState(this.clock));
+        return "none";
+    }
+    reachedToAlarmTime(): Action {
+        return "none";
+    }
+    snooze(): Action {
+        return "none";
+    }
+    elapseSnoozeTime(): Action {
+        this.clock.setState(new AlarmSoundingState(this.clock));
+        return "soundAlarm";
     }
 }
