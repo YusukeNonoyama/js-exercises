5fe8147b328824d9f36f97f764e637771e51686c
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-07-25 20:30:44 +0900

    ch13/ex04-ex06

---
+++ ch13/ex04/index.ts
@@ -0,0 +1,33 @@
+import { join } from 'path';
+import * as fsPromises from "node:fs/promises";
+
+export function fetchFirstFileSize(path: string, callback: Function) {
+    return fsPromises.readdir(path).then((files) => {
+        if (files.length === 0) {
+            throw new Error("no files found");
+        }
+        return fsPromises.stat(join(path, files[0])).then((stats) => {
+            return callback(stats.size);
+        });
+    });
+}
+
+export function fetchSumOfFileSizes(path: string, callback: Function) {
+    return fsPromises.readdir(path).then((files) => {
+        let total = 0;
+        const rest = [...files];
+        function iter(): any {
+            if (rest.length === 0) {
+                console.log("total", total);
+                console.log("total callback", callback(total));
+                return callback(total);
+            }
+            const next = rest.pop();
+            return fsPromises.stat(join(path, next as string)).then((stats) => {
+                total += stats.size;
+                return iter();
+            });
+        }
+        return iter();
+    });
+}
---
+++ ch13/ex05/index.ts
@@ -0,0 +1,62 @@
+function wait(msec: number) {
+    return new Promise((resolve) => setTimeout(resolve, msec));
+}
+
+function g1() {
+    // TODO: then のネストを無くしなさい
+    return wait(1000)
+        .then(() => { console.log("A"); })
+        .then(() => wait(2000))
+        .then(() => { console.log("B"); })
+        .then(() => wait(3000))
+        .then(() => { console.log("C"); })
+}
+
+function g2() {
+    // TODO: new Promise を使わないように書き換えなさい
+    // wait()のプロミスを使う
+    return wait(1000)
+        .then(() => console.log("A"))
+        .then(() => wait(2000))
+        .then(() => console.log("B"))
+        .then(() => wait(3000))
+        .then(() => console.log("C"))
+}
+
+
+type User = { id: number, name: string }
+
+function g3() {
+    // 以下2つの関数が存在するとします (中身は適当)
+    function fetchUser() {
+        return Promise.resolve({ id: 42, name: "John" });
+    }
+    function fetchUserFriends(user: User) {
+        return Promise.resolve([
+            { name: "Sam", id: 100 },
+            { name: "Bob", id: 1 },
+        ]);
+    }
+
+    // TODO: var, let, const による変数宣言を無くしなさい。async/awaitは使用しないこと。
+    // ネスト構造にすればよい
+    return fetchUser()
+        .then((user) => {
+            return fetchUserFriends(user).then((friends) => {
+                console.log(`${user.name} has ${friends.length} friends!`);
+            })
+        })
+}
+
+function g4() {
+    function someFunction() {
+        return 42;
+    }
+
+    // NOTE: この関数 g4 は Promise を返す必要があるものとする
+    // (利用しているフレームワークはライブラリがそういう関数を要求するとでも思って下さい)
+    // TODO: new Promise を使わないように書き換えなさい。async/awaitは使用しないこと。
+    let value = someFunction();
+    // Promise.resolve()を使う（newは使っていない）
+    return Promise.resolve(value);
+}
