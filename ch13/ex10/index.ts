f82bdffd0e5c46508cdc9e9bfc0d919ba107bc7c
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-08-03 09:52:04 +0900

    ch13/ex10

---
+++ ch13/ex10/index.ts
@@ -0,0 +1,15 @@
+import * as fsPromises from "node:fs/promises";
+import { join } from 'path';
+
+
+export async function fetchSumOfFileSizes(path: string, callback: Function) {
+    const files = await fsPromises.readdir(path)
+    const fileSizes = await Promise.all(
+        files.map(async (file) => {
+            const stats = await fsPromises.stat(join(path, file))
+            return callback(stats.size);
+        })
+    )
+    return fileSizes.reduce((acc, val) => acc + val, 0)
+
+}
