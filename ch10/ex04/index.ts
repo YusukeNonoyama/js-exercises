8eb93690591186544e0ca8dc0cb8a347b5c3e0f7
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-08-15 11:28:09 +0900

    ch10/ex04

--- ch10/ex04/index.ts
+++ ch10/ex04/index.ts
@@ -1,6 +1,6 @@
-import arrowFunction from "./module.ts" // defaultのインポート
-import { foo as fooRenamed , Bar} from "./module.ts" // 名前変更を伴うインポート
-import {a, b} from "./modules.ts"   // 再エクスポートしたファイルを参照
+import arrowFunction from "./module.ts" // defaultのインポート（関数名がなくてもエクスポートできる）
+import { foo as fooRenamed, Bar } from "./module.ts" // 名前変更を伴うインポート
+import { a, b } from "./modules.ts"   // 再エクスポートしたファイルを参照
 
 console.log(arrowFunction());
 console.log(fooRenamed());
@@ -8,6 +8,7 @@ console.log(new Bar().baz());
 console.log(a());
 console.log(b());
 
+// 結果：
 // Arrow function from models.ts
 // Function foo() from module.ts
 // Bar class method baz() from module.ts
