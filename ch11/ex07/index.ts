6c5e4c82ab0937a4d7a188a09345af184690a005
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-08-17 21:29:41 +0900

    ch11/ex07修正

---
+++ ch11/ex07/index.ts
@@ -0,0 +1,31 @@
+// 普通にコード書いて判定する場合
+
+const input = "(()(()))";
+const input2 = "((())";
+const pattern = /\(\)/;
+
+function hasPairsOfParenthethes(input: string) {
+    let currResultText = input;
+    let prevResultText = "";
+    let result: any = [];
+    while (currResultText !== "") {
+        result = currResultText.match(pattern)
+        if (currResultText.match(pattern))
+            currResultText = currResultText.slice(0, result.index) + currResultText.slice(result.index + 2, currResultText.length)
+        if (prevResultText === currResultText) {
+            return false
+        }
+        prevResultText = currResultText;
+    }
+    return true;
+}
+
+console.log(hasPairsOfParenthethes(input));
+console.log(hasPairsOfParenthethes(input2));
+
+// result = resultText.match(pattern)
+// resultText = resultText.slice(0, result.index) + resultText.slice(result.index + 2, resultText.length)
+// console.log(resultText)
+
+
+// console.log(input.match(pattern));
