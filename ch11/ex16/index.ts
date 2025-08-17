79b5d6df602d3bf4ef02ec57e1b19b8f5798a330
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-08-17 20:00:47 +0900

    ch11/ex16修正

--- ch11/ex16/index.ts
+++ ch11/ex16/index.ts
@@ -1,4 +1,11 @@
-export function retryWithExponentialBackoff(func: () => boolean, maxRetry: number, callback: (result: boolean) => boolean) {
+// * 受け取った関数 func を呼び出し、func が true を返せばそこで終了する
+// * func が false を返した場合は以下の待ち時間後に func 呼び出しをリトライする
+// * 待ち時間はfuncの呼び出し回数に応じて 1 秒, 2 秒, 4 秒, ...と 2 倍に増えていく
+// * maxRetry 回リトライしても成功しない場合はそこで終了する
+// * retryWithExponentialBackoffに対する呼び出しは即座に完了し、func の呼び出しは非同期に行われる
+// * func が true を返す、または maxRetry 回のリトライが失敗し終了する際、その結果(true/false)を引数として関数 callback が呼び出される
+
+export async function retryWithExponentialBackoff(func: () => boolean, maxRetry: number, callback: (result: boolean) => boolean) {
     let count = 0;
     // tryFunc()を定義してsetTimeout()内で繰り返し呼び出す
     function tryFunc() {
@@ -12,11 +19,10 @@ export function retryWithExponentialBackoff(func: () => boolean, maxRetry: numbe
             return;
         }
         const delay = 2 ** count * 1000;    // 1秒、2秒、4秒、、、
-        count++;
         setTimeout(tryFunc, delay);
+        count++;
     }
-    tryFunc();
-    // setTimeout()を呼び出した後にretryWithExponentialBackoff()は実行終了する  
+    await tryFunc();
 }
 
 
