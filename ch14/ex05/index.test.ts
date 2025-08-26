c680489e954a19478d39410ab5489fe1e684e1a0
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-08-26 21:40:35 +0900

    ch14/ex05

---
+++ ch14/ex05/index.test.ts
@@ -0,0 +1,19 @@
+import { templateToStringWithType } from "./index.ts";
+
+describe('templateToStringWithType()', () => {
+    test('補完値なし', () => {
+        expect(templateToStringWithType`no interpolations`).toBe("no interpolations");
+    });
+    test('補完値がstring', () => {
+        expect(templateToStringWithType`${"A"}`).toBe("string");
+    });
+    test('補完値がobject', () => {
+        expect(templateToStringWithType`${{ x: 1 }}`).toBe("object");
+    });
+    test('補完値がnumber', () => {
+        expect(templateToStringWithType`${100}`).toBe("number");
+    });
+    test('補完値と他の文字列含む', () => {
+        expect(templateToStringWithType`Type of the parameter is ${{ x: 1 }}.`).toBe("Type of the parameter is object.");
+    });
+});
---
+++ ch14/ex05/index.ts
@@ -0,0 +1,7 @@
+export const templateToStringWithType = (strings: any, ...input: any) => {
+    let result = strings[0];
+    for (let i = 0; i < input.length; i++) {
+        result += typeof input[i] + strings[i + 1];
+    }
+    return result;
+};
