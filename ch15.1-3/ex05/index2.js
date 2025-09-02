f7c7f6c3fb49f6b608a972587ea4754e1160dba2
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-09-02 14:28:26 +0900

    ch15.1-3/ex05

---
+++ ch15.1-3/ex05/index2.js
@@ -0,0 +1,8 @@
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.getElementById("1000").innerHTML = "Hello";
+// });
+
+window.addEventListener("load", () => {
+    document.getElementById("1000").innerHTML = "Hello";  
+})
