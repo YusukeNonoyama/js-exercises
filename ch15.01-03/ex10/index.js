3ad108f6438f077a96e2fb8cec7e47209acaa2b5
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-09-05 21:30:32 +0900

    ch15.01-03/ex10

---
+++ ch15.01-03/ex10/index.js
@@ -0,0 +1,21 @@
+const div_elem = document.getElementById("editor-front");
+div_elem.style.backgroundColor = "white";
+
+const input = document.getElementById("editor-back");
+
+
+div_elem.addEventListener("click", () => {
+    input.focus();
+})
+
+input.addEventListener("focus", () => {
+    div_elem.style.backgroundColor = "silver";
+});
+
+input.addEventListener("blur", () => {
+    div_elem.style.backgroundColor = "white";
+});
+
+input.addEventListener("input", () => {
+    div_elem.textContent = input.value;
+});
