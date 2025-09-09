22d65dd132e644ce4c48ecd82d34b85990684a4f
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-09-09 21:31:45 +0900

    ch12/ex01修正

--- ch12/ex01/index.ts
+++ ch12/ex01/index.ts
@@ -6,7 +6,6 @@ function counterIter(max: number) {
       console.log("counterIter: Symbol.iterator");
       return this;
     },
-
     next() {
       console.log("counterIter: next");
       if (c >= max + 1) {
@@ -23,7 +22,6 @@ function counterIter(max: number) {
     throw(e: ErrorConstructor) {
       console.log("counterIter: throw:", e);
       throw e;
-      return { value: undefined, done: true };
     },
   };
 }
@@ -43,91 +41,79 @@ function* counterGen(max: number) {
   }
 }
 
-const max = 5;
+const max = 3;
 
-// 「明示的にイテレータインタフェース のメソッドを呼んだり、」
 {
-  // counterIter()にてnext()を明示的に呼ぶ
+  console.log("=========明示的にnext()を呼ぶ： counterIter()")
   {
     const iter = counterIter(max);
-    console.log(iter.next());
-    console.log(iter.next());
-    console.log(iter.next());
-    console.log(iter.next());
-    console.log(iter.next());
-    console.log(iter.next());
-    console.log(iter.next());
+    let i = 0;
+    while (i < 5) {
+      console.log(iter.next());
+      i++;
+    }
+    // 結果
+    // counterIter
+    // counterIter: next
+    // { value: 1, done: false }
+    // counterIter: next
+    // { value: 2, done: false }
+    // counterIter: next
+    // { value: 3, done: false }
+    // counterIter: next
+    // { value: undefined, done: true }
+    // counterIter: next
+    // { value: undefined, done: true }
   }
-  // 結果
-  // counterIter
-  // counterIter: next
-  // { value: 1, done: false }
-  // counterIter: next
-  // { value: 2, done: false }
-  // counterIter: next
-  // { value: 3, done: false }
-  // counterIter: next
-  // { value: 4, done: false }
-  // counterIter: next
-  // { value: 5, done: false }
-  // counterIter: next
-  // { value: undefined, done: true }
-  // counterIter: next
-  // { value: undefined, done: true }
 
-  // counterGen()にてnext()を明示的に呼ぶ
+  console.log("=========明示的にnext()を呼ぶ： counterGen()")
   {
     const iterGen = counterGen(max);
-    console.log(iterGen.next());
-    console.log(iterGen.next());
-    console.log(iterGen.next());
-    console.log(iterGen.next());
-    console.log(iterGen.next());
-    console.log(iterGen.next());
-    console.log(iterGen.next());
+    let i = 0;
+    while (i < 5) {
+      console.log(iterGen.next());
+      i++;
+    }
+    // 結果
+    // counterGen
+    // counterGen: next
+    // { value: 1, done: false }
+    // counterGen: next
+    // { value: 2, done: false }
+    // counterGen: next
+    // { value: 3, done: false }
+    // counterGen: finally
+    // { value: undefined, done: true }
+    // { value: undefined, done: true }
   }
-  // 結果
-  // counterGen
-  // counterGen: next
-  // { value: 1, done: false }
-  // counterGen: next
-  // { value: 2, done: false }
-  // counterGen: next
-  // { value: 3, done: false }
-  // counterGen: next
-  // { value: 4, done: false }
-  // counterGen: next
-  // { value: 5, done: false }
-  // counterGen: finally
-  // { value: undefined, done: true }
-  // { value: undefined, done: true }
-  console.log("=====================================");
 
-  // counterIter()にてreturn()を明示的に呼ぶ
+  console.log("=========明示的にreturn()を呼ぶ： counterIter()")
   {
     const iter = counterIter(max);
-    console.log(iter.return(5)); // iteratorはreturnに引数を渡してそれiteration終了
+    console.log(iter.return(2)); // iteratorはreturnに引数を渡してそれiteration終了
+    // 結果
+    // counterIter
+    // counterIter: return: 2
+    // { value: 2, done: true }
   }
-  // 結果
-  // counterIter
-  // counterIter: return: 5
-  // { value: 5, done: true }
 
-  // counterGen()にてreturn()を明示的に呼ぶ
+  console.log("=========明示的にreturn()を呼ぶ： counterGen()")
   {
     const iterGen = counterGen(max);
     console.log(iterGen.next());
     console.log(iterGen.return());
-  }
-  // 結果
-  // counterGen
-  // counterGen: next
-  // { value: 1, done: false }
-  // counterGen: finally
-  // { value: undefined, done: true }　　// iterationの途中でないと出力はこの行だけ
-  console.log("=====================================");
+    // 結果
+    // counterGen
+    // counterGen: next
+    // { value: 1, done: false }
+    // counterGen: finally
+    // { value: undefined, done: true }
 
-  // counterIter()にてthrow()を明示的に呼ぶ
+    // iterationの途中でないと出力はこの行だけでfinallyは実行されない
+    // { value: undefined, done: true }　
+  }
+
+  console.log("=========明示的にthrow()を呼ぶ： counterIter()")
   {
     const iter = counterIter(max);
     try {
@@ -135,13 +121,13 @@ const max = 5;
     } catch (e) {
       console.log("error from counterIter()");
     }
+    // 結果
+    // counterIter
+    // counterIter: throw: [Function: Error] { stackTraceLimit: 10 }
+    // error from counterIter()
   }
-  // 結果
-  // counterIter
-  // counterIter: throw: [Function: Error] { stackTraceLimit: 10 }
-  // error from counterIter()
 
-  // counterGen()にてthrow()を明示的に呼ぶ
+  console.log("=========明示的にthrow()を呼ぶ： counterGen()")
   {
     const iterGen = counterGen(max);
     console.log(iterGen.next());
@@ -150,137 +136,178 @@ const max = 5;
     } catch (e) {
       console.log("error from counterGen()");
     }
+    // 結果
+    // counterGen
+    // counterGen: next
+    // { value: 1, done: false }
+    // counterGen: catch: [Function: Error] { stackTraceLimit: 10 }
+    // counterGen: finally
+    // error from counterGen()　　// iterationの途中でないと出力はこの行だけ
   }
-  // 結果
-  // counterGen
-  // counterGen: next
-  // { value: 1, done: false }
-  // counterGen: catch: [Function: Error] { stackTraceLimit: 10 }
-  // counterGen: finally
-  // error from counterGen()　　// iterationの途中でないと出力はこの行だけ
-  console.log("=====================================");
+
 }
 
-// 「間接的に呼んだりする」
 {
-  // counterIter()のnext()を間接的に呼ぶ
+  console.log("=========for-of ループを実行： counterIter()")
   {
-    const iter = counterIter(max);
+    const iter: any = counterIter(max);
     for (const value of iter) {
-      if (!value) continue;
       console.log(value);
     }
+    // 結果
+    // counterIter
+    // counterIter: Symbol.iterator
+    // counterIter: next
+    // 1
+    // counterIter: next
+    // 2
+    // counterIter: next
+    // 3
+    // counterIter: next
   }
-  // 結果
-  // counterIter
-  // counterIter: Symbol.iterator
-  // counterIter: next
-  // 1
-  // counterIter: next
-  // 2
-  // counterIter: next
-  // 3
-  // counterIter: next
-  // 4
-  // counterIter: next
-  // 5
-  // counterIter: next
 
-  // counterGen()のnext()を間接的に呼ぶ
+  console.log("=========for-of ループを実行： counterGen()")
   {
     const iterGen = counterGen(max);
     for (const value of iterGen) {
-      if (!value) continue;
       console.log(value);
     }
+    // counterGen
+    // counterGen: next
+    // 1
+    // counterGen: next
+    // 2
+    // counterGen: next
+    // 3
+    // counterGen: finally
   }
-  // 結果
-  // counterGen
-  // counterGen: next
-  // 1
-  // counterGen: next
-  // 2
-  // counterGen: next
-  // 3
-  // counterGen: next
-  // 4
-  // counterGen: next
-  // 5
-  // counterGen: finally
-  console.log("=====================================");
 
-  // counterIter()のreturn()を間接的に呼ぶ
+  console.log("=========for-of ループを実行途中で break： counterIter()")
   {
-    const iter = counterIter(max);
+    const iter: any = counterIter(max);
     for (const value of iter) {
-      if (!value) continue;
       if (value > 2) {
         break;
       }
       console.log(value);
     }
+    // 結果
+    // counterIter
+    // counterIter: Symbol.iterator
+    // counterIter: next
+    // 1
+    // counterIter: next
+    // 2
+    // counterIter: next
+    // counterIter: return: undefined
   }
-  // 結果
-  // counterIter
-  // counterIter: Symbol.iterator
-  // counterIter: next
-  // 1
-  // counterIter: next
-  // 2
-  // counterIter: next
-  // counterIter: return: undefined
-  console.log("========================");
 
-  // counterGen()のreturn()を間接的に呼ぶ
+
+  console.log("=========for-of ループを実行途中で break： counterGen()")
   {
     const iterGen = counterGen(max);
     for (const value of iterGen) {
-      if (!value) continue;
       if (value > 2) {
         break;
       }
       console.log(value);
     }
+    // 結果
+    // counterGen
+    // counterGen: next
+    // 1
+    // counterGen: next
+    // 2
+    // counterGen: next
+    // counterGen: finally
   }
-  // 結果
-  // counterGen
-  // counterGen: next
-  // 1
-  // counterGen: next
-  // 2
-  // counterGen: next
-  // counterGen: finally
 
-  // throw()を間接的に呼ぶ方法はわからなかった
+  console.log("=========for-of ループを実行中に例外発生： counterIter()")
+  {
+    const iter: any = counterIter(max);
+    try {
+      for (const value of iter) {
+        if (value > 2) {
+          throw Error("error thrown");
+        }
+        console.log(value);
+      }
+    } catch (e) {
+      console.log("catch");
+    } finally {
+      console.log("finally");
+    }
+    // 結果
+    //  counterIter
+    // counterIter: Symbol.iterator
+    // counterIter: next
+    // 1
+    // counterIter: next
+    // 2
+    // counterIter: next
+    // counterIter: return: undefined
+    // catch
+    // finally
+  }
+
+  console.log("=========for-of ループ実行中に例外発生： counterGen()")
+  {
+    const iterGen = counterGen(max);
+    try {
+      for (const value of iterGen) {
+        if (value > 2) {
+          throw Error("error thrown");
+        }
+        console.log(value);
+      }
+    } catch {
+      console.log("thrown");
+    } finally {
+      console.log("finally");
+    }
+    // 結果
+    // counterGen
+    // counterGen: next
+    // 1
+    // counterGen: next
+    // 2
+    // counterGen: next
+    // counterGen: finally
+    // thrown
+    // finally
+  }
 }
 
+
+
+// 以下は、旧内容
 // まとめ１
 // * 直接呼び出す場合、iteratorは期待通りの箇所が実行されるが、generatorはiterationの途中でない場合はコード実行が省略される箇所がある。
 // * 省略されるのは、generator関数内部の実行やfinally文。
 // * breakをすると、iteratorはreturn()が実行され、generatorはfinally節が実行される。
-console.log("========================");
+// console.log("========================");
 
 // 「ジェネレータ関数によって生成されたオブジェクトがイテレータインタフェースを満たしていることを確認する」
-{
-  const iterGen = counterGen(max);
-  console.log(iterGen.next);
-  console.log(iterGen.return);
-  console.log(iterGen.throw);
-  console.log(iterGen[Symbol.iterator]);
+// {
+// const iterGen = counterGen(max);
+// console.log(iterGen.next);
+// console.log(iterGen.return);
+// console.log(iterGen.throw);
+// console.log(iterGen[Symbol.iterator]);
 
-  // プロパティの取得
-  console.log(Object.getOwnPropertyNames(iterGen));
-  const proto = Object.getPrototypeOf(iterGen);
-  console.log(Object.getOwnPropertyNames(proto));
-  const proto2 = Object.getPrototypeOf(proto);
-  console.log(Object.getOwnPropertyNames(proto2));
-  const proto3 = Object.getPrototypeOf(proto2);
-  console.log(Object.getOwnPropertyNames(proto3));
-  const proto4 = Object.getPrototypeOf(proto3);
-  console.log(Object.getOwnPropertyNames(proto4));
-  const proto5 = Object.getPrototypeOf(proto4);
-  console.log(Object.getOwnPropertyNames(proto5));
-}
+// プロパティの取得
+// console.log(Object.getOwnPropertyNames(iterGen));
+// const proto = Object.getPrototypeOf(iterGen);
+// console.log(Object.getOwnPropertyNames(proto));
+// const proto2 = Object.getPrototypeOf(proto);
+// console.log(Object.getOwnPropertyNames(proto2));
+// const proto3 = Object.getPrototypeOf(proto2);
+// console.log(Object.getOwnPropertyNames(proto3));
+// const proto4 = Object.getPrototypeOf(proto3);
+// console.log(Object.getOwnPropertyNames(proto4));
+// const proto5 = Object.getPrototypeOf(proto4);
+// console.log(Object.getOwnPropertyNames(proto5));
+// }
 // 結果
 // イテレータインターフェースを満たしている。2階層下のプロトタイプに存在する。
 // [Function: next]
