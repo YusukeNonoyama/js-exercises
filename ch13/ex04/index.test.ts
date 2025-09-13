87247634f927f097ad4a7578032d35dda638b7ab
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-09-13 15:52:19 +0900

    ch13/ex04修正

--- ch13/ex04/index.test.ts
+++ ch13/ex04/index.test.ts
@@ -22,42 +22,14 @@ describe("fetchFirstFileSize()", () => {
     mkdirSync(dirPath);
     closeSync(openSync(filesPath, "w"));
     writeFileSync(filesPath, "123");
-    const result = await fetchFirstFileSize(
-      dirPath,
-      (statSize: number) => statSize,
-    ).catch((e) => {
-      return e;
-    });
+    const result = await fetchFirstFileSize(dirPath, (statSize: number) => statSize)
     expect(result).toEqual(3); // "123"で3 Bytes
   });
-  it("thenで呼び出したfetchFirstFileSize()からファイルサイズが返る", async () => {
-    const filesPath = join(dirPath, "index.ts");
-    mkdirSync(dirPath);
-    closeSync(openSync(filesPath, "w"));
-    writeFileSync(filesPath, "123");
-    const result = await fetchFirstFileSize(
-      dirPath,
-      (statSize: number) => statSize,
-    )
-      .then(() => fetchFirstFileSize(dirPath, (statSize: number) => statSize))
-      .catch((e) => {
-        return e;
-      });
-    expect(result).toEqual(3);
-  });
   it("fetchFirstFileSize()の入力パスが存在しない場合、プロミスは失敗する", async () => {
-    mkdirSync(dirPath);
     function f(statsSize: any) {
       return statsSize;
     }
     await expect(fetchFirstFileSize("no/existent/path", f)).rejects.toThrow();
-  });
-  it("fetchFirstFileSize()の入力パスにファイルが存在しない場合、プロミスは失敗する", async () => {
-    mkdirSync(dirPath);
-    function f(statsSize: any) {
-      return statsSize;
-    }
-    await expect(fetchFirstFileSize(dirPath, f)).rejects.toThrow();
   });
 });
 
@@ -83,15 +55,10 @@ describe("fetchSumOfFileSizes()", () => {
     mkdirSync(dirPath);
     closeSync(openSync(filesPath, "w"));
     writeFileSync(filesPath, "123");
-    const result = await fetchSumOfFileSizes(dirPath, (statSize: number) => {
-      // console.log(statSize);
-      return statSize;
-    }).catch((e) => {
-      return e;
-    });
+    const result = await fetchSumOfFileSizes(dirPath, (statSize: number) => statSize).catch(e => e);
     expect(result).toEqual(3);
   });
-  it("ファイルが1つの場合、fetchSumOfFileSizes()でファイルサイズが返る", async () => {
+  it("ファイルが3つの場合、fetchSumOfFileSizes()でファイルサイズが返る", async () => {
     mkdirSync(dirPath);
     const files = [
       ["index.ts", "123"],
@@ -108,17 +75,10 @@ describe("fetchSumOfFileSizes()", () => {
       return e;
     });
     // ファイルの中の合計文字数をカウント（一旦、簡単のため文字数＝バイト数の前提）
-    const totalLetterCount = files.reduce(
-      (acc, file) => acc + file[1].length,
-      0,
-    );
+    const totalLetterCount = files.reduce((acc, file) => acc + file[1].length, 0);
     expect(result).toEqual(totalLetterCount);
   });
   it("fetchSumOfFileSizes()の入力パスが存在しない場合、プロミスは失敗する", async () => {
-    mkdirSync(dirPath);
-    function f(statsSize: any) {
-      return statsSize;
-    }
-    await expect(fetchSumOfFileSizes("no/existent/path", f)).rejects.toThrow();
+    await expect(fetchSumOfFileSizes("no/existent/path", (statsSize: number) => statsSize)).rejects.toThrow();
   });
 });
