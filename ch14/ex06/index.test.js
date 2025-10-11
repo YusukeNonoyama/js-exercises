ebc6470ff1b2f71215fffdf45c386b4798a9f264
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-10-11 21:29:30 +0900

    ch14/ex06修正

--- ch14/ex06/index.test.js
+++ ch14/ex06/index.test.js
@@ -6,32 +6,22 @@ describe("createLoggingProxy", () => {
   let objLogArr;
   beforeEach(() => {
     obj = {
-      greet(name) {
-        return `Hey, ${name}`;
-      },
-      add(a, b) {
-        return a + b;
-      },
+      greet(name) { return `Hello, ${name}` },
+      add(a, b) { return a + b },
       value: 42,
     };
     [objProxy, objLogArr] = createLoggingProxy(obj);
   });
   test("methodが実行できる", () => {
-    expect(objProxy.greet("guys")).toBe("Hey, guys");
+    expect(objProxy.greet("world")).toBe("Hello, world");
     expect(objProxy.add(2, 3)).toBe(5);
   });
   test("呼び出したmethodの情報が配列に記録されている", () => {
-    objProxy.greet("dude");
+    objProxy.greet("world");
     objProxy.add(10, 20);
     expect(objLogArr.length).toBe(2);
-    expect(objLogArr[0]).toMatchObject({
-      methodName: "greet",
-      args: ["dude"],
-    });
-    expect(objLogArr[1]).toMatchObject({
-      methodName: "add",
-      args: [10, 20],
-    });
+    expect(objLogArr[0]).toMatchObject({ methodName: "greet", args: ["world"] });
+    expect(objLogArr[1]).toMatchObject({ methodName: "add", args: [10, 20] });
     const timeFormat = /^\d{1,2}:\d{2}:\d{2}$/;
     expect(objLogArr[0].invokedAt).toMatch(timeFormat);
     expect(objLogArr[1].invokedAt).toMatch(timeFormat);
