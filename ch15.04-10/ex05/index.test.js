e0aa2585b8cb73b0d072ed2e902665dd8541ede8
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-11-14 06:24:16 +0900

    修正

--- ch15.04-10/ex05/index.test.js
+++ ch15.04-10/ex05/index.test.js
@@ -1,14 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('border-width is applied correctly to inline-circle', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto("/ch15.04-10/ex05/index.html");
+});
 
+test('カスタム要素 inline-circle1 に border-width: 1px が正しく追加されている', async ({ page }) => {
     const borderWidth1 = await page.evaluate(() => {
         const el = document.querySelector('#inline-circle1');
         return getComputedStyle(el).borderWidth;
     });
     expect(borderWidth1).toBe('1px');
+});
 
+test('カスタム要素 inline-circle2 に border-width: 5px が正しく追加されている', async ({ page }) => {
     const borderWidth2 = await page.evaluate(() => {
         const el = document.querySelector('#inline-circle2');
         return getComputedStyle(el).borderWidth;
