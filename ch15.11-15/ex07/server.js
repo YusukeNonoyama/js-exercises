a09025a19200e2d98908f9fa4ec4d55432dd435c
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-11-24 21:36:13 +0900

    修正

--- ch15.11-15/ex07/server.js
+++ ch15.11-15/ex07/server.js
@@ -44,7 +44,10 @@ async function serveContentsHandler(url, _req, res) {
 // CSP のヘッダを返すミドルウェア
 function cspMiddleware(_url, req, res) {
   // TODO: CSP ヘッダを設定する
-  // res.setHeader("Content-Security-Policy", "TODO");
+  res.setHeader(
+    "Content-Security-Policy",
+    "script-src http://localhost:3000/hello.js 'unsafe-inline'",
+  );
   return true;
 }
 
