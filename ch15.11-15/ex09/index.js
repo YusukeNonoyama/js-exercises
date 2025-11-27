7fde9ada9b93fc10f7b87ccab9ef937c8b52a93f
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-11-28 07:44:56 +0900

    修正

--- ch15.11-15/ex09/index.js
+++ ch15.11-15/ex09/index.js
@@ -20,14 +20,12 @@ let animationId = null;
 // NOTE: download from https://soundeffect-lab.info/sound/button/mp3/decision1.mp3
 const sound = new Audio("/ch15.11-15/ex09/decision1.mp3");
 
-// ライフゲームのセル (true or false) をランダムに初期化する
 let grid = new Array(ROWS)
     .fill(null)
     .map(() =>
         new Array(COLS).fill(null).map(() => !!Math.floor(Math.random() * 2))
     );
 
-// grid を canvas に描画する
 function renderGrid(grid) {
     for (let row = 0; row < ROWS; row++) {
         for (let col = 0; col < COLS; col++) {
@@ -41,59 +39,14 @@ function renderGrid(grid) {
     }
 }
 
-// Life Game のルールに従ってセルを更新する
-function updateGrid(grid) {
-    // 新しいグリッドを作成
-    const nextGrid = grid.map((arr) => [...arr]);
-    for (let row = 0; row < ROWS; row++) {
-        for (let col = 0; col < COLS; col++) {
-            // 周囲のセルの生存数を数えて nextGrid[row][col] に true or false を設定する (実装してね)
-            const neighbours = [
-                [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]
-            ];
-            let numOfAlive = 0;
-            neighbours.forEach((nPos) => {
-                const pos = [row + nPos[0], col + nPos[1]];
-                // 計算対象のセルがグリッドの中のものだけを計算対象にして生存数を加算
-                if (pos.every(e => e >= 0 && e < 50)) {
-                    numOfAlive += grid[pos[0]][pos[1]]
-                }
-            })
-            // 対象セルが生存の場合の周囲のセルの生存数
-            // * ２未満なら or ４以上なら false(dead)
-            // * ２、３なら true(alive)
-            // 対象セイルが死の場合の周囲のセルの生存数
-            // * ３なら true(alive)
-            // * ３以外なら false(dead)
-            if (nextGrid[row][col] === true) {
-                if (numOfAlive >= 2 && numOfAlive <= 3) {
-                    nextGrid[row][col] = true;
-                } else {
-                    nextGrid[row][col] = false;
-                }
-            } else {
-                if (numOfAlive === 3) {
-                    nextGrid[row][col] = true;
-                } else {
-                    nextGrid[row][col] = false;
-                }
-            }
-        }
-    }
-    return nextGrid;
-}
-
-// canvas がクリックされたときの処理 (セルの値を反転する)
 canvas.addEventListener("click", function (evt) {
     const rect = canvas.getBoundingClientRect();
     const pos = { x: evt.clientX - rect.left, y: evt.clientY - rect.top };
-
     const row = Math.floor(pos.y / RESOLUTION);
     const col = Math.floor(pos.x / RESOLUTION);
-    grid[row][col] = !grid[row][col];
     sound.cloneNode().play();
-    renderGrid(grid);
 
+    // cクリックされたcanvasのセルをサーバーに送る
     socket.send(JSON.stringify({
         "type": "toggle",
         "row": row,
@@ -101,59 +54,30 @@ canvas.addEventListener("click", function (evt) {
     }))
 });
 
-// requestAnimationFrame によって一定間隔で更新・描画を行う
-// TODO: リフレッシュレートの高い画面では速く実行されてしまうため、以下を参考に更新頻度が常に一定となるようにしなさい
-// https://developer.mozilla.org/ja/docs/Web/API/Window/requestAnimationFrame
-let previousTime = 0;
-const interval = 100;
-function update(timestamp) {
-    if (!previousTime) {
-        previousTime = timestamp;
-    }
-    const elapsed = timestamp - previousTime;
-    if (elapsed > interval) {
-        previousTime = timestamp;
-        grid = updateGrid(grid);
-        renderGrid(grid);
-    }
-    animationId = requestAnimationFrame(update);
-}
+// アニメーション部分は不要のため削除（サーバーから１秒に10回送られる情報でgridを更新するだけ）
 
-btnStart.addEventListener("click", () => {
-    // 既にアニメーションが動いている場合は何もしない
-    if (animationId) {
-        return;
-    }
-    update();
-});
-
-btnPause.addEventListener("click", () => {
-    // アニメーションが停止している場合は何もしない
-    if (!animationId) {
-        return;
-    }
-    cancelAnimationFrame(animationId);
-    animationId = null;
-});
-
-renderGrid(grid);
-
-
-
+// start情報をサーバーに送る
 btnStart.addEventListener("click", () => {
     console.log("started");
     socket.send(JSON.stringify({ "type": "start" }));
 })
 
+// pause情報をサーバーに送る
 btnPause.addEventListener("click", () => {
     console.log("pause");
     socket.send(JSON.stringify({ "type": "pause" }));
 })
 
-socket.addEventListener("open", () => {
-    console.log("connected");
+// update情報を受け取ったときにgridを更新する
+socket.addEventListener("message", (event) => {
+    const msg = JSON.parse(event.data);
+
+    if (msg.type === "update") {
+        // console.log("message received for update: ", msg.grid);
+        const grid = msg.grid;
+        renderGrid(grid);
+    }
 })
 
-socket.addEventListener("message", (event) => {
-    console.log("message received: ", event);
-})
+// 初回レンダー
+renderGrid(grid);
