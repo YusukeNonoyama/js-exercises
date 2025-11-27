26afd19b65f1c1f54c0a73fb562e7f297f4d08e3
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-11-27 20:53:27 +0900

    修正

--- ch15.11-15/ex08/index.js
+++ ch15.11-15/ex08/index.js
@@ -1,20 +1,35 @@
 const btnCheck = document.querySelector("#ws-check");
 const btnSend = document.querySelector("#ws-send");
+const input1 = document.querySelector("#input-1");
+const input2 = document.querySelector("#input-2");
+const input3 = document.querySelector("#input-3");
+const input4 = document.querySelector("#input-4");
+const input5 = document.querySelector("#input-5");
+const label1 = document.querySelector("#label-1");
+const label2 = document.querySelector("#label-2");
+const label3 = document.querySelector("#label-3");
+const label4 = document.querySelector("#label-4");
+const label5 = document.querySelector("#label-5");
+
+// TODO: あとで削除
+btnCheck.addEventListener("click", () => {
+    console.log("connection state: ", socket.readyState);
+});
+
 
 let socket = new WebSocket("ws://localhost:3003");
 
 let requestId = 0;
 
-// リクエストIDとプロミスのresolve関数を保持するマップ
+// リクエストIDとリクエストのプロミスを保持するマップ
 const pendingRequests = new Map();
 
-// メッセージのハンドラ（リクエスト受け取り、レスポンス受け取り）
+// メッセージ受信時のハンドラ
 socket.addEventListener("message", (event) => {
     const msg = JSON.parse(event.data);
 
-    // メッセージがリクエストの場合
+    // メッセージがリクエストの場合、レスポンスを送り返す
     if (msg.type === "request") {
-        // レスポンスを送り返す
         socket.send(JSON.stringify({
             type: "response",
             requestId: msg.requestId,
@@ -23,11 +38,10 @@ socket.addEventListener("message", (event) => {
         return;
     }
 
-    // メッセージがレスポンスの場合、タイムアウトしていたら実行しない
+    // メッセージがレスポンスの場合、該当するIDのプロミスを解決する、タイムアウトしていたら実行しない
     if (msg.type === "response" && pendingRequests.has(msg.requestId)) {
-        // 該当するIDのプロミスを解決する
         pendingRequests.get(msg.requestId).resolve(msg.data);
-        pendingRequests.delete(msg.requestId); // プロミスが解決したら消す（エラー時に再び呼ばれないよ）
+        pendingRequests.delete(msg.requestId);
         return;
     }
 });
@@ -40,40 +54,60 @@ socket.addEventListener("close", () => {
     }
 });
 
-// TODO: あとで削除
-btnCheck.addEventListener("click", () => {
-    console.log("connection state: ", socket.readyState);
-});
-
 // 送信ボタンを押すとsendRequestが非同期で呼ばれる
 btnSend.addEventListener("click", async () => {
-    const id = ++requestId;
+    console.log("message sent...")
+    // const id = ++requestId;
 
-    try {
-        console.log(`request sent for id ${id}`);
-        const response = await sendRequest("Yuss", id);
-        console.log(`message received for id ${id}:`, response);
-    } catch (e) {
-        console.log(`error occurred for id ${id}: `, e);
-    }
+    const promises = [
+        sendRequest(input1.value.trim(), 1),
+        sendRequest(input2.value.trim(), 2),
+        sendRequest(input3.value.trim(), 3),
+        sendRequest(input4.value.trim(), 4),
+        sendRequest(input5.value.trim(), 5)
+    ]
+
+    Promise.allSettled(promises).then(results => {
+        label1.textContent = results[0].value || results[0].reason
+        label2.textContent = results[1].value || results[1].reason
+        label3.textContent = results[2].value || results[2].reason
+        label4.textContent = results[3].value || results[3].reason
+        label5.textContent = results[4].value || results[4].reason
+        console.log(results[0]);
+        console.log(results[1]);
+        console.log(results[2]);
+        console.log(results[3]);
+        console.log(results[4]);
+    })
+
+    // 連続クリックをidでトラックできるようにしたコード
+    // try {
+    //     console.log(`request sent for id ${id}`);
+    //     const response = await sendRequest(input, id);
+    //     console.log(`message received for id ${id}:`, response);
+    // } catch (e) {
+    //     console.log(`error occurred for id ${id}: `, e);
+    // }
+
+
 });
 
-function sendRequest(data, reqId) {
+function sendRequest(data, requestId) {
     return new Promise((resolve, reject) => {
-        pendingRequests.set(reqId, { resolve, reject });
+        pendingRequests.set(requestId, { resolve, reject });
 
         // タイムアウト時にrejectしマップから対象IDの情報を消す。
         setTimeout(() => {
-            if (pendingRequests.has(reqId)) {
-                pendingRequests.delete(reqId);
+            if (pendingRequests.has(requestId)) {
+                pendingRequests.delete(requestId);
                 reject("timeout");
             }
-        }, 5000);
+        }, 8000);
 
         socket.send(JSON.stringify({
             type: "request",
-            requestId: reqId,
-            data: data
+            requestId,
+            data
         }));
     });
 }
