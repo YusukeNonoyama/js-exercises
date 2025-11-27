3051b630cd90426872a949aa01e8dd3717864778
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-11-27 21:45:59 +0900

    修正

--- ch15.11-15/ex09/server.js
+++ ch15.11-15/ex09/server.js
@@ -12,10 +12,10 @@ const wss = new WebSocketServer({ port });
 
 // ライフゲームのセル (true or false) をランダムに初期化する
 let grid = new Array(ROWS)
-.fill(null)
-.map(() =>
-  new Array(COLS).fill(null).map(() => !!Math.floor(Math.random() * 2))
-);
+  .fill(null)
+  .map(() =>
+    new Array(COLS).fill(null).map(() => !!Math.floor(Math.random() * 2))
+  );
 // 停止状態
 let paused = true;
 
@@ -62,6 +62,36 @@ function updateGrid(grid) {
     for (let col = 0; col < COLS; col++) {
       // 周囲のセルの生存数を数えて nextGrid[row][col] に true or false を設定する
       //（15.04-10.10の実装を利用）
+      const neighbours = [
+        [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]
+      ];
+      let numOfAlive = 0;
+      neighbours.forEach((nPos) => {
+        const pos = [row + nPos[0], col + nPos[1]];
+        // 計算対象のセルがグリッドの中のものだけを計算対象にして生存数を加算
+        if (pos.every(e => e >= 0 && e < 50)) {
+          numOfAlive += grid[pos[0]][pos[1]]
+        }
+      })
+      // 対象セルが生存の場合の周囲のセルの生存数
+      // * ２未満なら or ４以上なら false(dead)
+      // * ２、３なら true(alive)
+      // 対象セイルが死の場合の周囲のセルの生存数
+      // * ３なら true(alive)
+      // * ３以外なら false(dead)
+      if (nextGrid[row][col] === true) {
+        if (numOfAlive >= 2 && numOfAlive <= 3) {
+          nextGrid[row][col] = true;
+        } else {
+          nextGrid[row][col] = false;
+        }
+      } else {
+        if (numOfAlive === 3) {
+          nextGrid[row][col] = true;
+        } else {
+          nextGrid[row][col] = false;
+        }
+      }
     }
   }
   return nextGrid;
