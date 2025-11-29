d0edd79424ec60164dc3f2e10dc8e963a0b129b4
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-11-29 20:41:43 +0900

    修正

--- ch15.11-15/ex10/utils/cruncher.js
+++ ch15.11-15/ex10/utils/cruncher.js
@@ -2,6 +2,71 @@ console.log("cruncher");
 
 addEventListener("message", (event) => {
     // console.log("message recieved at WorkerGlobalObject: ", event);
-    postMessage(`request: ${event.data}`);
+    // postMessage(`request: ${event.data}`);
+    console.log(`type: ${event.data.type}`);
+    console.log(`image: ${event.data.image}`);
+    // console.log(`data: ${event.data.value}`);
+
+    const data = event.data.value;
+
+    const width = Number(event.data.image.width);
+    const height = Number(event.data.image.height);
+    console.log("wh:", width, height)
+
+    // 5x5 ガウシアンカーネル
+    const gaussianKernel = [
+        [1, 4, 6, 4, 1],
+        [4, 16, 24, 16, 4],
+        [6, 24, 36, 24, 6],
+        [4, 16, 24, 16, 4],
+        [1, 4, 6, 4, 1]
+    ];
+    const kernelSize = 5;
+    const kernelOffset = Math.floor(kernelSize / 2); // 開始点のオフセット
+    const kernelSum = 256; // カーネル要素の合計
+
+    // (r, g, b, α) × 画素分の型付き配列
+    const outputData = new Uint8ClampedArray(data.length);
+
+    // 開始点のオフセット分の除いたすべての画素をループ
+    for (let y = kernelOffset; y < height - kernelOffset; y++) {
+        for (let x = kernelOffset; x < width - kernelOffset; x++) {
+            let rSum = 0;
+            let gSum = 0;
+            let bSum = 0;
+
+            // 計算対象の画素からカーネルのサイズ分をループしてr, g, b をそれぞれ計算
+            for (let ky = -kernelOffset; ky <= kernelOffset; ky++) {
+                for (let kx = -kernelOffset; kx <= kernelOffset; kx++) {
+                    // (x, y)：計算対象の画素座標 
+                    //（kx, ky）：カーネルの座標（カーネル中心が（0, 0））
+                    //（px, py）：このループの変数
+                    const px = x + kx;
+                    const py = y + ky;
+                    // カーネルのウエイトを参照
+                    const weight = gaussianKernel[ky + kernelOffset][kx + kernelOffset];
+                    // カーネル内の１要素をweightをかけて加算
+                    const i = (py * width + px) * 4;
+                    rSum += data[i] * weight;
+                    gSum += data[i + 1] * weight;
+                    bSum += data[i + 2] * weight;
+                }
+            }
+
+            // 計算した値を画素の値を代入
+            const j = (y * width + x) * 4;
+            outputData[j] = rSum / kernelSum;
+            outputData[j + 1] = gSum / kernelSum;
+            outputData[j + 2] = bSum / kernelSum;
+            outputData[j + 3] = data[j + 3]; // Alpha はそのまま
+        }
+    }
+    const outputImageData = new ImageData(outputData, width, height);
+
+    postMessage({
+        type: "response",
+        value: outputImageData,
+    })
+
 })
 
