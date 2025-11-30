66265690c9f3f02a0b401d889c8171b21815802f
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-11-30 10:41:47 +0900

    修正

--- ch15.11-15/ex10/index.js
+++ ch15.11-15/ex10/index.js
@@ -1,13 +1,5 @@
 const dataCruncher = new Worker("utils/cruncher.js");
 
-const btnWorker = document.querySelector("#worker-start");
-
-btnWorker.addEventListener("click", () => {
-    dataCruncher.postMessage("Can you see me?")
-});
-
-
-
 document.getElementById("image").addEventListener("change", (event) => {
     const file = event.target.files[0];
     if (!file) {
@@ -23,22 +15,21 @@ document.getElementById("image").addEventListener("change", (event) => {
 
     img.addEventListener("load", () => {
         const originalCanvas = document.getElementById("original");
-        // const filteredCanvas = document.getElementById("filtered");
+        const filteredCanvas = document.getElementById("filtered");
         const originalCtx = originalCanvas.getContext("2d");
-        // const filteredCtx = filteredCanvas.getContext("2d");
+        const filteredCtx = filteredCanvas.getContext("2d");
 
         originalCanvas.width = img.width;
         originalCanvas.height = img.height;
-        // filteredCanvas.width = img.width;
-        // filteredCanvas.height = img.height;
+        filteredCanvas.width = img.width;
+        filteredCanvas.height = img.height;
 
         originalCtx.drawImage(img, 0, 0);
 
         const imageData = originalCtx.getImageData(0, 0, img.width, img.height);
-
-        // これがWorkerの入力
         const data = imageData.data;
 
+        // Workerへ処理する画像を送信
         dataCruncher.postMessage({
             type: "request",
             value: data,
@@ -48,74 +39,10 @@ document.getElementById("image").addEventListener("change", (event) => {
             }
         })
 
-        // const width = img.width;
-        // const height = img.height;
-
-        // // 5x5 ガウシアンカーネル
-        // const gaussianKernel = [
-        //     [1, 4, 6, 4, 1],
-        //     [4, 16, 24, 16, 4],
-        //     [6, 24, 36, 24, 6],
-        //     [4, 16, 24, 16, 4],
-        //     [1, 4, 6, 4, 1]
-        // ];
-        // const kernelSize = 5;
-        // const kernelOffset = Math.floor(kernelSize / 2); // 開始点のオフセット
-        // const kernelSum = 256; // カーネル要素の合計
-
-        // // (r, g, b, α) × 画素分の型付き配列
-        // const outputData = new Uint8ClampedArray(data.length);
-
-        // // 開始点のオフセット分の除いたすべての画素をループ
-        // for (let y = kernelOffset; y < height - kernelOffset; y++) {
-        //     for (let x = kernelOffset; x < width - kernelOffset; x++) {
-        //         let rSum = 0;
-        //         let gSum = 0;
-        //         let bSum = 0;
-
-        //         // 計算対象の画素からカーネルのサイズ分をループしてr, g, b をそれぞれ計算
-        //         for (let ky = -kernelOffset; ky <= kernelOffset; ky++) {
-        //             for (let kx = -kernelOffset; kx <= kernelOffset; kx++) {
-        //                 // (x, y)：計算対象の画素座標 
-        //                 //（kx, ky）：カーネルの座標（カーネル中心が（0, 0））
-        //                 //（px, py）：このループの変数
-        //                 const px = x + kx;
-        //                 const py = y + ky;
-        //                 // カーネルのウエイトを参照
-        //                 const weight = gaussianKernel[ky + kernelOffset][kx + kernelOffset];
-        //                 // カーネル内の１要素をweightをかけて加算
-        //                 const i = (py * width + px) * 4;
-        //                 rSum += data[i] * weight;
-        //                 gSum += data[i + 1] * weight;
-        //                 bSum += data[i + 2] * weight;
-        //             }
-        //         }
-
-        //         // 計算した値を画素の値を代入
-        //         const j = (y * width + x) * 4;
-        //         outputData[j] = rSum / kernelSum;
-        //         outputData[j + 1] = gSum / kernelSum;
-        //         outputData[j + 2] = bSum / kernelSum;
-        //         outputData[j + 3] = data[j + 3]; // Alpha はそのまま
-        //     }
-        // }
-
-        // TODO: ここで imageData.data を参照して outputData に結果を格納
-        // const outputImageData = new ImageData(outputData, img.width, img.height);
-        // filteredCtx.putImageData(outputImageData, 0, 0);
+        // Workerからresponseが返ってきたら処理後の画像をcanvasへ描画
         dataCruncher.addEventListener("message", (event) => {
             if (event.data.type === "response") {
-                console.log(event.data);
-
-                const filteredCanvas = document.getElementById("filtered");
-                const filteredCtx = filteredCanvas.getContext("2d");
-
-                filteredCanvas.width = img.width;
-                filteredCanvas.height = img.height;
                 filteredCtx.putImageData(event.data.value, 0, 0);
-
-                // reader.readAsDataURL(file);
-
             }
         });
     });
