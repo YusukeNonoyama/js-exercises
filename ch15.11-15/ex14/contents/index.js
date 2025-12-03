3434e18d990a7430e5cf55c37e6e8695321a4be3
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-12-03 12:45:03 +0900

    追加

--- ch15.11-15/ex14/contents/index.js
+++ ch15.11-15/ex14/contents/index.js
@@ -5,6 +5,7 @@ const messageContainer = document.getElementById("message-container");
 button.addEventListener("click", (e) => {
   e.preventDefault();
 
+  // 通信ボタンがクリックされたらボタンを無効化
   button.disabled = "disabled";
 
   getMessageFromServer();
@@ -16,14 +17,18 @@ async function getMessageFromServer() {
   messageContainer.appendChild(messageElement);
 
   // TODO: ここにサーバーとのやり取り等を実装しなさい
+
+  // Server-Sentイベント作成
   const message = new EventSource("/message");
+
+  // サーバーからの送信を受け取る
   message.addEventListener("message", (event) => {
 
-
+    // 内容を表示
     const response = JSON.parse(event.data);
-
     messageElement.textContent = response.value;
 
+    // サーバーからの送信が終了したらServer-Sentイベントを終了（doneプロパティがtrue）、同時に送信ボタン有効化
     if (response.done) {
       message.close();
       button.disabled = null;
