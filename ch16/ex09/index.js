85c0339766fbc46b9a6d9c352ee3ac433acdee38
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2025-12-26 19:22:30 +0900

    追加

---
+++ ch16/ex09/index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import path from "path";
+import fs from "fs";
+
+const app = express();
+
+const rootDirectory = path.resolve(process.argv[2] || "ch16/ex09/original/");
+const port = parseInt(process.argv[3], 10) || 8000;
+
+// パスに一致するGETメソッドのみに対応
+app.get("/test/mirror", (req, res) => {
+    res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+    res.writeHead(200);
+
+    res.write(`${req.method} ${req.originalUrl} HTTP/${req.httpVersion}\r\n`);
+
+    const headers = req.rawHeaders;
+    for (let i = 0; i < headers.length; i += 2) {
+        res.write(`${headers[i]}:${headers[i + 1]}\r\n`);
+    }
+
+    res.write("\r\n");
+    req.pipe(res);
+});
+
+// ホストへの全てのメソッドで全てのパスを受け取る
+app.use((req, res) => {
+    let filename = req.path.substring(1);
+
+    filename = filename.replace(/\.\.\//g, "");
+
+    const absolutePath = path.resolve(rootDirectory, filename);
+
+    let type;
+    switch (path.extname(absolutePath)) {
+        case ".html":
+        case ".htm":
+            type = "text/html";
+            break;
+        case ".js":
+            type = "text/javascript";
+            break;
+        case ".css":
+            type = "text/css";
+            break;
+        case ".png":
+            type = "image/png";
+            break;
+        case ".txt":
+            type = "text/plain";
+            break;
+        default:
+            type = "application/octet-stream";
+    }
+
+    const stream = fs.createReadStream(absolutePath);
+
+    stream.once("readable", () => {
+        res.setHeader("Content-Type", type);
+        res.status(200);
+        stream.pipe(res);
+    });
+
+    stream.on("error", (err) => {
+        res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+        res.status(404).end(err.message);
+    });
+});
+
+app.listen(port, () => {
+    console.log("Listening on port", port);
+});
