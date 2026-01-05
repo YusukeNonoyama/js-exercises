7c8381f89356aead4cedff4071fb914483cd673e
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2026-01-06 06:43:43 +0900

    追加

--- ch16/ex07/index.ts
+++ ch16/ex07/index.ts
@@ -1,18 +1,23 @@
-import * as fs from "fs";
+import { lstat } from "fs/promises";
 
-export function checkEntry(file: string) {
-  let stats: fs.Stats | undefined;
+export async function checkEntry(
+  file: string,
+): Promise<"file" | "directory" | "something else"> {
+  let stats;
   try {
-    stats = fs.statSync(file);
-  } catch (e) {
+    // 非同期にstatsを取得（lstat()だとシンボリックリンクの場合はリンク先を辿らない）
+    stats = await lstat(file);
+  } catch {
     throw new Error("invalid input");
   }
 
+  // statsの種類に応じて返り値を変える
   if (stats.isFile()) {
     return "file";
   } else if (stats.isDirectory()) {
     return "directory";
   } else {
+    // シンボリックリンクの場合
     return "something else";
   }
 }
