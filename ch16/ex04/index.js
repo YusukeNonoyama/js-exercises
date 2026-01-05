fe6f739e16b16c0fb6f716308d186010f18ebae9
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2026-01-05 20:19:37 +0900

    追加

--- ch16/ex04/index.js
+++ ch16/ex04/index.js
@@ -1,10 +1,10 @@
-import fs from "fs";
+import fs from "fs/promises";
 import iconv from 'iconv-lite';
 
-// Shift_JIS のままバイナリとして読み込む
-const buffer = fs.readFileSync("ch16/ex04/hello.txt");
+// エンコード非指定でバイナリのバッファとして読み込む
+const buffer = await fs.readFile("ch16/ex04/hello.txt");
 
-// Shift_JIS → UTF-8 にデコード
+// バッファを読み込んでShift_JIS → UTF-8 にデコード
 const text = iconv.decode(buffer, 'shift_jis');
 
 console.log(text);
--- ch16/ex04/index.md
+++ ch16/ex04/index.md
@@ -1,5 +1,5 @@
 ## メモ
 ### SHIFT-JIS、ANSIとは
-* メモ帳でエンド―ドをANSIを指定して保存するとSHIFT-JISになる
+* メモ帳でエンコードでANSIを指定して保存するとSHIFT-JISになる
   * ANSIはWindows におけるローカルな文字コードの総称で、国・地域ごとに異なるエンコーディングになっている。日本の場合はSHIFT-JIS
 * https://hissori.com/character-encoding/
