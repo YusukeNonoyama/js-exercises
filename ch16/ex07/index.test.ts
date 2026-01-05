54453b55b117a53db0ee806f034cef49a647b9af
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2026-01-06 06:48:18 +0900

    追加

--- ch16/ex07/index.test.ts
+++ ch16/ex07/index.test.ts
@@ -10,14 +10,10 @@ describe("test checkEntry()", () => {
     expect(fileType).toEqual("directory");
   });
   it("fixture is a symbolic link", async () => {
-    const fileType = await checkEntry(
-      "ch16/ex07/fixture/linkToFileFixture.txt",
-    );
+    const fileType = await checkEntry("ch16/ex07/fixture/linkToFileFixture.txt");
     expect(fileType).toEqual("something else");
   });
   it("fixture is invalid", async () => {
-    await expect(checkEntry("fixture/noExistenceFixture")).rejects.toThrow(
-      Error,
-    );
+    await expect(checkEntry("fixture/noExistenceFixture")).rejects.toThrow(Error);
   });
 });
