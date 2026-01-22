d37ee3582ad54fe58f902b6a29b8805788aca5db
YusukeNonoyama <yuusuke.nonoyama@jp.ricoh.com>
2026-01-22 21:38:10 +0900

    追加

---
+++ ch17/ex06/index.md
@@ -0,0 +1,6 @@
+- ソースマップを出力もコマンド
+  - `npx webpack --mode=production --devtool=source-map ./ch17/ex06/index.js -o ./ch17/ex06/dist`
+- ソースコードファイルがどのように表示されるかを確認しなさい。
+  ![alt text]({02BAB3CA-A30E-4318-BFB8-9FCCE436E5B5}.png)
+- バンドルしたコードの実行中に、バンドル前のソースコードファイルに基づいたブレークポイントの設定や変数の値の確認等のデバッグが可能か確認しなさい。
+  ![alt text]({E7ABAE81-7333-4BA1-858D-CE5EBA714BCF}.png)
---
+++ ch17/ex06/util.js
@@ -0,0 +1,61 @@
+export function renderGrid(grid, ROWS, COLS, ctx, RESOLUTION) {
+  for (let row = 0; row < ROWS; row++) {
+    for (let col = 0; col < COLS; col++) {
+      const cell = grid[row][col];
+      ctx.beginPath();
+      ctx.rect(col * RESOLUTION, row * RESOLUTION, RESOLUTION, RESOLUTION);
+      ctx.fillStyle = cell ? 'black' : 'white';
+      ctx.fill();
+      ctx.stroke();
+    }
+  }
+  return [grid, ctx];
+}
+
+export function updateGrid(grid, ROWS, COLS) {
+  // 新しいグリッドを作成
+  const nextGrid = grid.map((arr) => [...arr]);
+  for (let row = 0; row < ROWS; row++) {
+    for (let col = 0; col < COLS; col++) {
+      // 周囲のセルの生存数を数えて nextGrid[row][col] に true or false を設定する (実装してね)
+      const neighbours = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+      ];
+      let numOfAlive = 0;
+      neighbours.forEach((nPos) => {
+        const pos = [row + nPos[0], col + nPos[1]];
+        // 計算対象のセルがグリッドの中のものだけを計算対象にして生存数を加算
+        if (pos.every((e) => e >= 0 && e < 50)) {
+          numOfAlive += grid[pos[0]][pos[1]];
+        }
+      });
+      // 対象セルが生存の場合の周囲のセルの生存数
+      // * ２未満なら or ４以上なら false(dead)
+      // * ２、３なら true(alive)
+      // 対象セイルが死の場合の周囲のセルの生存数
+      // * ３なら true(alive)
+      // * ３以外なら false(dead)
+      if (nextGrid[row][col] === true) {
+        if (numOfAlive >= 2 && numOfAlive <= 3) {
+          nextGrid[row][col] = true;
+        } else {
+          nextGrid[row][col] = false;
+        }
+      } else {
+        if (numOfAlive === 3) {
+          nextGrid[row][col] = true;
+        } else {
+          nextGrid[row][col] = false;
+        }
+      }
+    }
+  }
+  return nextGrid;
+}
